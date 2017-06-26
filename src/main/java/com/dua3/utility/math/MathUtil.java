47a043979debd2e378b8030298840666468d7f6e
Axel Howind <axel@dua3.com>
2017-06-26 22:39:56 +0200

    add MathUtil, tests

---
+++ src/main/java/com/dua3/utility/math/MathUtil.java
@@ -0,0 +1,298 @@
+package com.dua3.utility.math;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ *
+ * @author axel
+ */
+public final class MathUtil {
+
+    /**
+     * Utility class - private constructor.
+     */
+    private MathUtil() {
+    }
+
+    /**
+     * Round to decimal places.
+     *
+     * @param x
+     * @param n
+     * @return x rounded to n decimal places
+     */
+    public static double round(double x, int n) {
+        if (x == 0 || Double.isNaN(x) || Double.isInfinite(x)) {
+            return x;
+        }
+
+        int m = Math.abs(n);
+        long f = 1;
+        while (m > 0) {
+            f *= 10;
+            m--;
+        }
+        double scale = n >= 0 ? f : 1.0 / f;
+        return Math.round(x * scale) / scale;
+    }
+
+    public static double roundToPrecision(double x, int p) {
+        if (x == 0 || Double.isNaN(x) || Double.isInfinite(x)) {
+            return x;
+        }
+
+        int n = p - ilog10(Math.abs(x)) - 1;
+        return round(x, n);
+    }
+
+    public static String toDecimalString(double xm, int digits) {
+        StringBuilder sb = new StringBuilder();
+
+        // sign
+        final String sign;
+        if (xm < 0) {
+            sign = "-";
+            xm = -xm;
+        } else {
+            sign = "";
+        }
+
+        // avoid error due to imprecise decimal representation
+        final double korr = pow10(-(digits + 1));
+        xm += korr;
+
+        // integer part
+        final int xi = (int) round(xm, digits);
+        sb.append(xi);
+        xm -= xi;
+
+        boolean showSign = xi != 0;
+        if (digits > 0) {
+            sb.append(".");
+            int k = 10;
+            for (int i = 0; i < digits; i++) {
+                final int d = ((int) (xm * k)) % 10;
+                sb.append((char) ('0' + d));
+                k *= 10;
+                showSign |= d != 0;
+            }
+        }
+
+        return showSign ? sign + sb.toString() : sb.toString();
+    }
+
+    public static double pow10(int i) {
+        if (i == Integer.MIN_VALUE) {
+            // -Integer.MIN_VALUE will overflow
+            return 1.0 / (10 * pow10(Integer.MAX_VALUE));
+        } else if (i < 0) {
+            return 1.0 / pow10(-i);
+        }
+
+        double result = 1.0;
+        while (i-- > 0) {
+            result *= 10.0;
+        }
+        return result;
+    }
+
+    /**
+     * Calculate ceil(log10(x)).
+     *
+     * @param x
+     * @return ceil(log10(x))
+     */
+    public static int ilog10(double x) {
+        if (x <= 0 || Double.isNaN(x) || Double.isInfinite(x)) {
+            throw new IllegalArgumentException(Double.toString(x));
+        } else if (x >= 1.0) {
+            int i = 0;
+            while ((x /= 10.0) >= 1) {
+                i++;
+            }
+            return i;
+        } else {
+            int i = -1;
+            while ((x *= 10.0) < 1) {
+                i--;
+            }
+            return i;
+        }
+    }
+
+    /**
+     * Find root of function.
+     *
+     * The root finding uses a modified secant algorithm. The values given as staring points should
+     * be rough approximations of the root, ie the algorithm might fail if the second order
+     * derivative changes sign in the interval [xa,root] or [xb,root].
+     *
+     * @param f
+     *            the function for which the root shall be calculated
+     * @param xa
+     *            first starting point
+     * @param xb
+     *            second starting point
+     * @param eps
+     *            desired accuracy
+     * @return the calculated root or Double.NaN if none is found
+     */
+    public static double findRoot(DoubleUnaryOperator f, double xa, double xb, double eps) {
+
+        final int maxIterations = 50;
+
+        if (xb < xa) {
+            double tmp = xa;
+            xa = xb;
+            xb = tmp;
+        }
+
+        double ya = f.applyAsDouble(xa);
+        double yb = f.applyAsDouble(xb);
+
+        int iteration = 0;
+        do {
+            if (iteration++ > maxIterations) {
+                return Double.NaN;
+            }
+
+            if (ya == 0.0) {
+                return xa;
+            }
+
+            if (yb == 0.0) {
+                return xb;
+            }
+
+            double xc = xa - ya * (xb - xa) / (yb - ya);
+
+            if (Double.isNaN(xc)) {
+                return Double.NaN;
+            }
+
+            if (xc < xa) {
+                xb = xa;
+                yb = ya;
+                xa = xc;
+                ya = f.applyAsDouble(xa);
+            } else if (xc > xb) {
+                xa = xb;
+                ya = yb;
+                xb = xc;
+                yb = f.applyAsDouble(xb);
+            } else if (2 * (xc - xa) < (xb - xa)) {
+                xb = xc;
+                yb = f.applyAsDouble(xb);
+            } else {
+                xa = xc;
+                ya = f.applyAsDouble(xa);
+            }
+        } while (xb - xa > eps);
+        return (xa + xb) / 2.0;
+    }
+
+    /**
+     * Find all roots within the given interval.
+     *
+     * The function is evaluated a fixed number of times to find starting values for root finding.
+     *
+     * @param f
+     * @param x0
+     * @param x1
+     * @param steps
+     * @param eps
+     * @return list of all calculated roots
+     */
+    public static List<Double> findRootsInInterval(DoubleUnaryOperator f, double x0, double x1, int steps, double eps) {
+        if (x0 == x1) {
+            throw new IllegalArgumentException("Empty interval.");
+        }
+
+        if (x0 > x1) {
+            double tmp = x0;
+            x0 = x1;
+            x1 = tmp;
+        }
+
+        ArrayList<Double> roots = new ArrayList<>();
+        double step = (x1 - x0) / steps;
+        double xa = x0, ya = f.applyAsDouble(xa);
+        double dya = Double.NaN;
+        for (int i = 0; i < steps; i++) {
+            double x = x0 + (i + 1) * (x1 - x0) / steps;
+            double y = f.applyAsDouble(x);
+            double dy = ya - y;
+
+            if (ya * y <= 0 || dya * dy <= 0) {
+                try {
+                    final double root = findRoot(f, xa, x, eps);
+                    if (!Double.isNaN(root) && root >= x0 - eps && root <= x1 + eps) {
+                        roots.add(root);
+                        // avoid finding the same root twice
+                        x += step / 2.0;
+                        y = f.applyAsDouble(x);
+                        // keep value of dy
+                    }
+                } catch (Exception e) {
+                    // nop
+                }
+            }
+            xa = x;
+            ya = y;
+            dya = dy;
+        }
+
+        return roots;
+    }
+
+    public static boolean isIntegral(Double a) {
+        return !Double.isNaN(a) && a == Math.floor(a);
+    }
+
+    /**
+     * Calculate the greatest common divisor.
+     * The greatest common divisor is calculated using the Euclidean algorithm.
+     * 
+     * @param a
+     *            first argument
+     * @param b
+     *            second argument
+     * @return greatest common divisor of a and b.
+     */
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long h = a % b;
+            a = b;
+            b = h;
+        }
+        return a;
+    }
+
+    /**
+	 * Clip argument to range.
+	 * @param min minimal value
+	 * @param max maximum value
+	 * @param arg argument
+	 * @return 
+	 * <ul>
+	 * <li>	min, if arg < min
+	 * <li> max, if arg > max
+	 * <li> else arg
+	 * </ul>
+	 */
+	public static int clamp(int min, int max, int arg) {
+		assert min<=max;
+		
+		if (arg<min) {
+			return min;
+		}
+		
+		if (arg>max) {
+			return max;
+		}
+		
+		return arg;
+	}
+}
---
+++ src/test/java/com/dua3/math/MathUtilTest.java
@@ -0,0 +1,157 @@
+package com.dua3.math;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dua3.utility.math.MathUtil;
+
+/**
+ *
+ * @author axel
+ */
+public class MathUtilTest {
+
+    public MathUtilTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        // nop
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        // nop
+    }
+
+    @Before
+    public void setUp() {
+        // nop
+    }
+
+    @After
+    public void tearDown() {
+        // nop
+    }
+
+    /**
+     * Test of round method, of class MathUtil.
+     */
+    @Test
+    public void testRound() {
+        System.out.println("round");
+        double[][] tests = {
+                // arg, round(arg,2), round(arg,-2)
+                { 0, 0, 0 }, { 1.234, 1.23, 0 }, { -1.234, -1.23, 0 }, { -1234.567, -1234.57, -1200 }, };
+
+        for (double[] test : tests) {
+            double arg = test[0];
+            double expected = test[1];
+            double result = MathUtil.round(arg, 2);
+            assertEquals(expected, result, 1e-15);
+        }
+    }
+
+    /**
+     * Test of toDecimalString method, of class MathUtil.
+     */
+    @Test
+    public void testToDecimalString() {
+        System.out.println("toDecimalString");
+        assertEquals("1.0", MathUtil.toDecimalString(1.0, 1));
+    }
+
+    /**
+     * Test of pow10 method, of class MathUtil.
+     */
+    @Test
+    public void testPow10() {
+        System.out.println("pow10");
+        assertEquals(0.01, MathUtil.pow10(-2), 1e-15);
+        assertEquals(0.1, MathUtil.pow10(-1), 1e-15);
+        assertEquals(1.0, MathUtil.pow10(0), 1e-15);
+        assertEquals(10.0, MathUtil.pow10(1), 1e-15);
+        assertEquals(100.0, MathUtil.pow10(2), 1e-15);
+    }
+
+    /**
+     * Test of ilog10 method, of class MathUtil.
+     */
+    @Test
+    public void testIlog10() {
+        System.out.println("ilog10");
+        assertEquals(-4, MathUtil.ilog10(0.00099), 1e-15);
+        assertEquals(-3, MathUtil.ilog10(0.001), 1e-15);
+        assertEquals(-3, MathUtil.ilog10(0.0099), 1e-15);
+        assertEquals(-2, MathUtil.ilog10(0.01), 1e-15);
+        assertEquals(-2, MathUtil.ilog10(0.099), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.2), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.3), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.4), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.5), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.6), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.7), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.8), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.9), 1e-15);
+        assertEquals(-1, MathUtil.ilog10(0.99), 1e-15);
+        assertEquals(0, MathUtil.ilog10(1.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(2.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(3.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(4.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(5.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(6.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(7.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(8.0), 1e-15);
+        assertEquals(0, MathUtil.ilog10(9.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(10.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(20.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(30.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(40.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(50.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(60.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(70.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(80.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(90.0), 1e-15);
+        assertEquals(1, MathUtil.ilog10(99.0), 1e-15);
+        assertEquals(2, MathUtil.ilog10(100.0), 1e-15);
+    }
+
+    /**
+     * Test of roundToPrecision method, of class MathUtil.
+     */
+    @Test
+    public void testRoundToPrecision() {
+        System.out.println("roundToPrecision");
+        assertEquals(1.2, MathUtil.roundToPrecision(1.2345, 2), 1e-10);
+        assertEquals(1.235, MathUtil.roundToPrecision(1.2345, 4), 1e-10);
+    }
+
+    /**
+     * Test of findRoot method, of class MathUtil.
+     */
+    @Test
+    public void testFindRoot() {
+        System.out.println("findRoot");
+        double result = MathUtil.findRoot(x -> (x - 5) * (x + 2), 4, 10, 1e-15);
+        double expResult = 5;
+        assertEquals(expResult, result, 1e-15);
+    }
+
+    /**
+     * Test of findRootsInIntervall method, of class MathUtil.
+     */
+    @Test
+    public void testFindRootsInIntervall() {
+        System.out.println("findRootsInIntervall");
+        List<Double> result = MathUtil.findRootsInInterval(x -> 3 * x * (x - 2.0 / 3.0), -10.5, +10.5, 20, 1e-15);
+        assertEquals(2, result.size());
+        assertEquals(0, result.get(0), 1e-15);
+        assertEquals(2.0 / 3.0, result.get(1), 1e-15);
+    }
+
+}
