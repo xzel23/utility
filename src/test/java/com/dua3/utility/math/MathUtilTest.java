867109d68b0969a64ba28b2549b1cacfc0fcdea9
Axel Howind <axel@dua3.com>
2017-08-02 15:11:58 +0200

    moved test classes to correct package

--- src/test/java/com/dua3/utility/math/MathUtilTest.java
+++ src/test/java/com/dua3/utility/math/MathUtilTest.java
@@ -1,9 +1,11 @@
-package com.dua3.math;
+package com.dua3.utility.math;
+
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
