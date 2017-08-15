80e3599adb97671a3eae99de41af3d887a9c9526
Axel Howind <axel@dua3.com>
2017-08-15 14:07:01 +0200

    main method to update markdown test results

--- src/test/java/com/dua3/utility/text/MarkDownTest.java
+++ src/test/java/com/dua3/utility/text/MarkDownTest.java
@@ -1,6 +1,9 @@
 package com.dua3.utility.text;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,10 +15,19 @@ import org.junit.Test;
 public class MarkDownTest {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
+        System.err.println("WARNING! This will overwrite expected unit test results!!!\nEnter 'YES' to continue.");
+        if (!"YES".equals(new BufferedReader(new InputStreamReader(System.in)).readLine().trim())) {
+            System.err.println("aborted.");
+            System.exit(1);
+        }
+
         String html = getHtml();
-//      try (PrintStream out = new PrintStream(path)) {
-//          out.println(html);
-//      }
+        Path htmlPath = Paths.get(MarkDownTest.class.getResource("syntax.html").toURI());
+        try (PrintStream out = new PrintStream(htmlPath.toFile())) {
+            out.print(html);
+            System.out.println("Wrote new expected unit test result.");
+            System.err.println("Copy "+htmlPath+" to resources folder to permanently update tests.");
+        }
     }
 
     @Test
