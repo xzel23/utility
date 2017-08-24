7f5fedf8cb6c6bef53ba26bfda55085bc4bb0ae6
Axel Howind <axel@dua3.com>
2017-08-24 16:03:57 +0200

    : implement remaining markdown styles
    Task-Url:

--- src/main/java/com/dua3/utility/text/MarkDownStyle.java
+++ src/main/java/com/dua3/utility/text/MarkDownStyle.java
@@ -69,10 +69,10 @@ public enum MarkDownStyle {
 
         m.put(MarkDownStyle.BLOCK_QUOTE.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.BULLET_LIST.name(), attr -> TextAttributes.of());
-        m.put(MarkDownStyle.CODE.name(), attr -> TextAttributes.of());
+        m.put(MarkDownStyle.CODE.name(), attr -> TextAttributes.of(Pair.of(TextAttributes.FONT_FAMILY, TextAttributes.FONT_FAMILY_VALUE_MONOSPACE)));
         m.put(MarkDownStyle.DOCUMENT.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.EMPHASIS.name(), attr -> TextAttributes.of(Pair.of(TextAttributes.FONT_STYLE, TextAttributes.FONT_STYLE_VALUE_ITALIC)));
-        m.put(MarkDownStyle.FENCED_CODE_BLOCK.name(), attr -> TextAttributes.of());
+        m.put(MarkDownStyle.FENCED_CODE_BLOCK.name(), attr -> TextAttributes.of(Pair.of(TextAttributes.FONT_FAMILY, TextAttributes.FONT_FAMILY_VALUE_MONOSPACE)));
         m.put(MarkDownStyle.HARD_LINE_BREAK.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.HEADING.name(), attr -> TextAttributes.of(
                 Pair.of(TextAttributes.FONT_SIZE, getFontSizeForHeading.apply((Integer)attr.args.getOrDefault(MarkDownStyle.ATTR_HEADING_LEVEL, 1))),
@@ -82,7 +82,7 @@ public enum MarkDownStyle {
         m.put(MarkDownStyle.HTML_BLOCK.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.HTML_INLINE.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.IMAGE.name(), attr -> TextAttributes.of());
-        m.put(MarkDownStyle.INDENTED_CODE_BLOCK.name(), attr -> TextAttributes.of());
+        m.put(MarkDownStyle.INDENTED_CODE_BLOCK.name(), attr -> TextAttributes.of(Pair.of(TextAttributes.FONT_FAMILY, TextAttributes.FONT_FAMILY_VALUE_MONOSPACE)));
         m.put(MarkDownStyle.LINK.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.LIST_ITEM.name(), attr -> TextAttributes.of());
         m.put(MarkDownStyle.ORDERED_LIST.name(), attr -> TextAttributes.of());
