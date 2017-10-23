6f09af48761fb2b817fc567e6322cec8b5f35602
Axel Howind <axel@dua3.com>
2017-10-23 12:12:53 +0200

    add missing final modifier

--- src/test/java/com/dua3/utility/logging/LogWindowTest.java
+++ src/test/java/com/dua3/utility/logging/LogWindowTest.java
@@ -5,22 +5,22 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 public class LogWindowTest {
-    
-    private static Logger LOG = Logger.getLogger(LogWindowTest.class.getSimpleName());
-    
+
+    private static final Logger LOG = Logger.getLogger(LogWindowTest.class.getSimpleName());
+
     public static void main(String[] args) {
         LOG.info("Creating window");
         LogWindow w = new LogWindow();
         w.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
+
         w.addLogger(LOG);
         w.setSize(600, 400);
         w.setVisible(true);
-        
+
         LOG.info("initialized.");
         LOG.warning("Is everything alright?");
         LOG.severe("I dunno.");
         LOG.fine("lalalala.");
     }
-    
+
 }
