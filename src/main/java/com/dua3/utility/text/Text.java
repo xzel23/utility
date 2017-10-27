97d18c5be0314cd14c8b2864e1aa48eba0918f01
Axel Howind <axel@dua3.com>
2017-10-27 13:19:51 +0200

    Text: toString(), empty(), region(int,int), cleanup source

--- src/main/java/com/dua3/utility/text/Text.java
+++ src/main/java/com/dua3/utility/text/Text.java
@@ -9,39 +9,49 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.dua3.utility.lang.LangUtil;
+
 public class Text extends AbstractList<String> {
 
-    private final List<String> lines;
+    private static final char NEWLINE = '\n';
+    private static final String NEWLINE_STR = "" + '\n';
+
+    public static Text empty() {
+        return new Text();
+    }
 
     public static Text load(Path path, Charset cs) throws IOException {
         try (BufferedReader reader = Files.newBufferedReader(path, cs)) {
             Text text = new Text();
             for (;;) {
                 String line = reader.readLine();
-                if (line == null)
+                if (line == null) {
                     break;
+                }
                 text.addUnchecked(line);
             }
             return text;
         }
     }
 
-    public Text(List<String> lines) {
-        this.lines = new ArrayList<>(lines);
+    private static void checkNoLineBreaks(String line) {
+        if (containsLineBreaks(line)) {
+            throw new IllegalArgumentException("line must not contain linebreaks");
+        }
     }
+
+    private static boolean containsLineBreaks(String line) {
+        return line.indexOf(NEWLINE) >= 0;
+    }
+
+    private final List<String> lines;
 
     public Text() {
         lines = new ArrayList<>();
     }
 
-    @Override
-    public String get(int index) {
-        return lines.get(index);
-    }
-
-    @Override
-    public int size() {
-        return lines.size();
+    public Text(List<String> lines) {
+        this.lines = new ArrayList<>(lines);
     }
 
     @Override
@@ -50,24 +60,40 @@ public class Text extends AbstractList<String> {
         return addUnchecked(line);
     }
 
-    private boolean addUnchecked(String line) {
-        return lines.add(line);
-    }
-
     public boolean addText(String text) {
-        for (String s: text.split("\n")) {
+        for (String s : text.split(NEWLINE_STR)) {
             addUnchecked(s);
         }
         return true;
     }
 
-    private static void checkNoLineBreaks(String line) {
-        if (containsLineBreaks(line)) {
-            throw new IllegalArgumentException("line must not contains linebreaks");
-        }
+    @Override
+    public String get(int index) {
+        return lines.get(index);
     }
 
-    private static boolean containsLineBreaks(String line) {
-        return line.indexOf('\n')>=0;
+    public Text region(int begin, int end) {
+        LangUtil.check(begin >= 0 && begin < end && end <= lines.size(), "invalid region from {} to {}", begin, end);
+        return new Text(lines.subList(begin, end));
+    }
+
+    @Override
+    public int size() {
+        return lines.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String sep = "";
+        for (String line : lines) {
+            sb.append(sep).append(line);
+            sep = NEWLINE_STR;
+        }
+        return sb.toString();
+    }
+
+    private boolean addUnchecked(String line) {
+        return lines.add(line);
     }
 }
