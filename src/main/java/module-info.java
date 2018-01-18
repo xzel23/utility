1738cd70aee5103320562aa5537c732f9178b58b
Axel Howind <axel@dua3.com>
2018-01-18 07:53:10 +0100

    jdk9

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,14 +11,11 @@ module com.dua3.utility {
 	exports com.dua3.utility.jfx;
 	exports com.dua3.utility.lang;
 	exports com.dua3.utility.swing;
+	exports com.dua3.utility.logging;
 	exports com.dua3.utility;
 	exports com.dua3.utility.io;
 
-	requires java.datatransfer;
 	requires java.desktop;
 	requires java.logging;
-	requires java.xml;
-	requires javafx.web;
-	requires junit;
-	requires slf4j.api;
+	requires org.junit;
 }
