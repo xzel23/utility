ecd2ce20b185a3c2ddee2e6cc9b4c87b8f6fec47
Axel Howind <axel@dua3.com>
2018-02-15 17:56:45 +0100

    Jigsaw

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.dua3.utility {
+    exports com.dua3.utility;
+    exports com.dua3.utility.io;
+    exports com.dua3.utility.jfx;
+    exports com.dua3.utility.swing;
+    exports com.dua3.utility.lang;
+    exports com.dua3.utility.math;
+    exports com.dua3.utility.text;
+
+    requires javafx.controls;
+    requires javafx.web;
+    requires java.xml;
+    requires java.desktop;
+    requires org.apache.logging.log4j;
+}
