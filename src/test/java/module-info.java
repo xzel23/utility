bb678e406c220ba90a4db0e518cb6ebb11e6a083
Axel Howind <axel@dua3.com>
2018-09-28 10:36:53 +0200

    add module-info for test

---
+++ src/test/java/module-info.java
@@ -0,0 +1,12 @@
+module com.dua3.utility.test {
+    requires com.dua3.utility;
+
+    requires java.xml;
+    requires java.desktop;
+    requires java.logging;
+
+    requires javafx.controls;
+    requires javafx.web;
+
+    requires junit;
+}
