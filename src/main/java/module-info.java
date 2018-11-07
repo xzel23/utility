3e48a28d72daf89a9ab7cc682b254ba1e1197820
Axel Howind <axel@dua3.com>
2018-11-07 08:48:48 +0100

    add NamedParameterStatement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,10 +6,12 @@ module com.dua3.utility {
     exports com.dua3.utility.lang;
     exports com.dua3.utility.math;
     exports com.dua3.utility.text;
+    exports com.dua3.utility.db;
 
     requires java.xml;
     requires java.desktop;
     requires java.logging;
+    requires java.sql;
 
     requires javafx.controls;
     requires javafx.web;
