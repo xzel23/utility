f1a600a8a244392e4d9012e8908b13e205dde711
Axel Howind <axel@dua3.com>
2019-01-05 17:55:35 +0100

    add test

--- src/test/java/com/dua3/utility/test/text/TextUtilTest.java
+++ src/test/java/com/dua3/utility/test/text/TextUtilTest.java
@@ -3,6 +3,9 @@ package com.dua3.utility.test.text;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import com.dua3.utility.Pair;
 import com.dua3.utility.text.TextUtil;
 
 public class TextUtilTest {
@@ -16,5 +19,24 @@ public class TextUtilTest {
         
         assertEquals(expected, actual);
     }
-    
+
+    private static final List<Pair<String, byte[]>> byteArrayHexStringTestData = List.of(
+			Pair.of("00", new byte[]{0x00}),
+			Pair.of("a0cafe", new byte[]{(byte)0xa0, (byte)0xca, (byte)0xfe})
+        );
+        
+    @Test
+    public void testByteArrayToHexString() {
+		for (var entry: byteArrayHexStringTestData) {
+			assertEquals(entry.first, TextUtil.byteArrayToHexString(entry.second));
+		}
+    }
+
+    @Test
+    public void testHexStringToByteArray() {
+		for (var entry: byteArrayHexStringTestData) {
+			assertArrayEquals(entry.second, TextUtil.hexStringToByteArray(entry.first));
+		}
+    }
+
 }
