867e8fe3474a499cae325e625d1138fade7745e5
Axel Howind <axel@dua3.com>
2019-01-07 18:09:07 +0100

    add utility.swing as a separate library again

---
+++ utility.swing/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.dua3.utility.swing {
+    exports com.dua3.utility.swing;
+
+    requires com.dua3.utility;
+
+    requires java.datatransfer;
+    requires java.desktop;
+    requires java.logging;
+}
