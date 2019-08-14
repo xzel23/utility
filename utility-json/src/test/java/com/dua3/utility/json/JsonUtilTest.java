627d344c8452743198363067c959bad6b6dc1a8d
Axel Howind <axel@dua3.com>
2019-08-14 14:39:15 +0200

    import

--- utility-json/src/test/java/com/dua3/utility/json/JsonUtilTest.java
+++ utility-json/src/test/java/com/dua3/utility/json/JsonUtilTest.java
@@ -1,6 +1,7 @@
 package com.dua3.utility.json;
 
 import com.dua3.utility.io.IOUtil;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URL;
@@ -13,12 +14,12 @@ class JsonUtilTest {
     private final URL resourceUrl = getClass().getResource("test.json");
     private final Path resourcePath = IOUtil.toPath(resourceUrl);
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testReadUrl() throws IOException {
         JsonUtil.read(resourceUrl);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testReadPath() throws IOException {
         JsonUtil.read(resourcePath);
     }
