b3d20aa3291eda2227a02cc1d71536803435b6ac
Axel Howind <axel@dua3.com>
2020-01-13 13:25:37 +0100

    add Platform class

---
+++ utility/src/main/java/com/dua3/utility/lang/Platform.java
@@ -0,0 +1,66 @@
+package com.dua3.utility.lang;
+
+import java.util.Locale;
+
+public enum Platform {
+    LINUX,
+    MACOS,
+    WINDOWS,
+    UNKNOWN;
+
+    private static final Platform detected = determinePlatform();
+    
+    private static Platform determinePlatform() {
+        String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ROOT);
+        if ((os.contains("mac")) || (os.contains("darwin"))) {
+            return Platform.MACOS;
+        }
+        if (os.contains("windows")) {
+            return Platform.WINDOWS;
+        }
+        if (os.contains("linux")) {
+            return Platform.LINUX;
+        }
+        return Platform.UNKNOWN;
+    }
+
+    /**
+     * Get the detected platform that the program runs on.
+     * @return the detected platform
+     */
+    public static Platform getCurrentPlatform() {
+        return detected;
+    }
+
+    /**
+     * Check if current platform is Windows.
+     * @return true if currently running under a Windows operating system
+     */
+    public static boolean isWindows() {
+        return detected==WINDOWS;
+    }
+
+    /**
+     * Check if current platform is Linux.
+     * @return true if currently running under a Linux operating system
+     */
+    public static boolean isLinux() {
+        return detected==LINUX;
+    }
+
+    /**
+     * Check if current platform is MacOS.
+     * @return true if currently running under a MacOS operating system
+     */
+    public static boolean isMacOS() {
+        return detected==MACOS;
+    }
+
+    /**
+     * Check if current platform is unknown.
+     * @return true if currently running under an unknown operating system
+     */
+    public static boolean isUnknown() {
+        return detected==UNKNOWN;
+    }
+}
