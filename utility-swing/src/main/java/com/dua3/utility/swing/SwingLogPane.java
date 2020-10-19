8672eec82979260a3179ba95658a4c0ba859080c
Axel Howind <axel@dua3.com>
2020-10-19 10:01:25 +0200

    SwingLogPane: add colors

--- utility-swing/src/main/java/com/dua3/utility/swing/SwingLogPane.java
+++ utility-swing/src/main/java/com/dua3/utility/swing/SwingLogPane.java
@@ -1,13 +1,16 @@
 package com.dua3.utility.swing;
 
+import com.dua3.utility.data.Color;
 import com.dua3.utility.lang.RingBuffer;
 import com.dua3.utility.logging.LogEntry;
 import com.dua3.utility.logging.LogListener;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
-import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
@@ -19,29 +22,67 @@ public class SwingLogPane extends JPanel implements LogListener {
     private final JTable table;
     private final AbstractTableModel model;
     private final JScrollPane scrollPane;
+    private final Function<LogEntry, Color> colorize;
 
-    private static class Column {
-        final String name;
-        final Class<?> type;
-        final Function<LogEntry,?> get;
-        
-        public <T> Column(String name, Class<T> type, Function<LogEntry,T> get) {
-            this.name = name;
-            this.type = type;
-            this.get = get;
+    private static Color defaultColorize(LogEntry entry) {
+        switch (entry.category()) {
+            case SEVERE:
+                return Color.RED;
+            case WARNING:
+                return Color.DARKORANGE;
+            case INFO:
+                return Color.DARKBLUE;
+            case DEBUG:
+                return Color.BLACK;
+            case TRACE:
+                return  Color.DARKGRAY;
+            default:
+                return Color.BLACK;
         }
     }
     
-    private static final Column[] COLUMNS = {
-      new Column("Time", LocalDateTime.class, LogEntry::time),
-      new Column("Level", String.class, LogEntry::level),
-      new Column("Message", String.class, LogEntry::text)      
+    class LogEntryFieldCellRenderer extends DefaultTableCellRenderer {
+        private final LogEntry.Field f;
+
+        public LogEntryFieldCellRenderer(LogEntry.Field f) {
+            this.f = f;
+        }
+
+        public void setValue(Object value) {
+            java.awt.Color color; 
+            
+            Object v;
+            if (value instanceof LogEntry) {
+                LogEntry entry = (LogEntry) value;
+                color = SwingUtil.toAwtColor(colorize.apply(entry));
+                v = entry.get(f);
+            } else {
+                color = java.awt.Color.BLACK;
+                v = value;
+            }
+            
+            setForeground(color);
+            super.setValue(v);
+        }
+    }
+    
+    private static final LogEntry.Field[] COLUMNS = {
+            LogEntry.Field.TIME,
+            LogEntry.Field.LEVEL,
+            LogEntry.Field.CATEGORY,
+            LogEntry.Field.TEXT
     };
     
     private final RingBuffer<LogEntry> buffer = new RingBuffer<>(DEFAULT_CAPACITY);
-    
+
     public SwingLogPane() {
+        this(SwingLogPane::defaultColorize);
+    }
+    
+    public SwingLogPane(Function<LogEntry, Color> colorize) {
         super(new BorderLayout());
+        
+        this.colorize = Objects.requireNonNull(colorize);
         
         model = new AbstractTableModel() {
             @Override
@@ -55,22 +96,29 @@ public class SwingLogPane extends JPanel implements LogListener {
             }
 
             @Override
-            public Object getValueAt(int rowIndex, int columnIndex) {
-                return COLUMNS[columnIndex].get.apply(buffer.get(rowIndex));
+            public LogEntry getValueAt(int rowIndex, int columnIndex) {
+                return buffer.get(rowIndex);
             }
 
             @Override
             public String getColumnName(int column) {
-                return COLUMNS[column].name;
+                return COLUMNS[column].toString();
             }
 
             @Override
             public Class<?> getColumnClass(int columnIndex) {
-                return COLUMNS[columnIndex].type;
+                return LogEntry.Field.class;
             }
         };
         
         table = new JTable(model);
+
+        TableColumnModel columnModel = table.getColumnModel();
+        for (int i = 0; i < columnModel.getColumnCount(); i++) {
+            TableCellRenderer r = new LogEntryFieldCellRenderer(COLUMNS[i]);
+            columnModel.getColumn(i).setCellRenderer(r);
+        }
+        
         scrollPane = new JScrollPane(table);
         add(scrollPane);
     }
--- utility/src/main/java/com/dua3/utility/logging/LogEntry.java
+++ utility/src/main/java/com/dua3/utility/logging/LogEntry.java
@@ -1,8 +1,17 @@
 package com.dua3.utility.logging;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 public interface LogEntry {
+    enum Field {
+        CATEGORY,
+        LEVEL,
+        TIME,
+        TEXT,
+        STACK_TRACE
+    };
+    
     Category category();
 
     String level();
@@ -12,4 +21,21 @@ public interface LogEntry {
     String text();
 
     String[] stacktrace();
+    
+    default Object get(Field f) {
+        switch (f) {
+            case CATEGORY:
+                return category();
+            case LEVEL:
+                return level();
+            case TIME:
+                return time();
+            case TEXT:
+                return text();
+            case STACK_TRACE:
+                return stacktrace();
+            default:
+                throw new IllegalArgumentException("no such field: "+f);
+        }
+    }
 }
