3d075dba281aaa745c8aad15ae8db3adc3e45494
Axel Howind <axel@dua3.com>
2020-10-26 10:32:03 +0100

    com.dua3.utility.logging was missing in module-info.java

--- utility/src/main/java/module-info.java
+++ utility/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module dua3_utility {
     exports com.dua3.utility.data;
     exports com.dua3.utility.io;
     exports com.dua3.utility.lang;
+    exports com.dua3.utility.logging;
     exports com.dua3.utility.math;
     exports com.dua3.utility.options;
     exports com.dua3.utility.text;
