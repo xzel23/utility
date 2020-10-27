6bd773d812f3658745203e404503d2ad64b77fdc
Axel Howind <axel@dua3.com>
2020-10-27 17:24:56 +0100

    LogListener.filter()

--- utility/src/main/java/com/dua3/utility/logging/LogListener.java
+++ utility/src/main/java/com/dua3/utility/logging/LogListener.java
@@ -1,8 +1,21 @@
 package com.dua3.utility.logging;
 
+import java.util.function.Predicate;
+
 @FunctionalInterface
 public interface LogListener {
 
+    static LogListener filter(LogListener listener, Predicate<LogEntry> pass) {
+        return entry -> new LogListener() {
+            @Override
+            public void entry(LogEntry entry) {
+                if (pass.test(entry)) {
+                    listener.entry(entry);
+                }
+            }
+        };
+    }
+    
     void entry(LogEntry entry);
 
 }
