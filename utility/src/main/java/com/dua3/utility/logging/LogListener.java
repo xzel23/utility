fde622ef277d3fc122a293fdc6100aeebd18bb3f
Axel Howind <axel@dua3.com>
2020-10-28 14:29:50 +0100

    fix LogListener.filter()

--- utility/src/main/java/com/dua3/utility/logging/LogListener.java
+++ utility/src/main/java/com/dua3/utility/logging/LogListener.java
@@ -6,12 +6,9 @@ import java.util.function.Predicate;
 public interface LogListener {
 
     static LogListener filter(LogListener listener, Predicate<LogEntry> pass) {
-        return entry -> new LogListener() {
-            @Override
-            public void entry(LogEntry entry) {
-                if (pass.test(entry)) {
-                    listener.entry(entry);
-                }
+        return entry -> {
+            if (pass.test(entry)) {
+                listener.entry(entry);
             }
         };
     }
