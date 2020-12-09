d3519b74de41d06f824362a56b1372f1d9b3b4e8
Axel Howind <axel@dua3.com>
2020-12-09 10:39:22 +0100

    add javadoc

--- utility/src/main/java/com/dua3/utility/cmd/CmdException.java
+++ utility/src/main/java/com/dua3/utility/cmd/CmdException.java
@@ -6,15 +6,29 @@ import java.util.Optional;
  * Exception class to throw when command line arguments do not match the options defined by the command line parser.
  */
 public class CmdException extends IllegalStateException {
+    /**
+     * Exception thrown when a parameter argument's String value could not be converted to the target type. 
+     */
     public static class ConversionException extends CmdException {
         Option<?> option;String parameter;
 
+        /**
+         * Constructor.
+         * @param option the option the argument belongds to
+         * @param parameter the parameter value as String
+         * @param e the parent exception
+         */
         public ConversionException(Option<?> option, String parameter, Exception e) {
             super("invalid value passed to "+option.name()+": "+parameter, e);
             this.option = option;
             this.parameter = parameter;
         }
 
+        /**
+         * Constructor.
+         * @param option the option the argument belongds to
+         * @param parameter the parameter value as String
+         */
         public ConversionException(Option<?> option, String parameter) {
             super("invalid value passed to "+option.name()+": "+parameter);
             this.option = option;
