ac106c3586b7d289c820bb4eb3decfdd1f11a768
Axel Howind <axel@dua3.com>
2021-04-19 17:27:01 +0200

    - constants for componentwise positive/negative infinity - min(),max(),angle() - make scalarProduct() static

--- utility/src/main/java/com/dua3/utility/math/Vec2d.java
+++ utility/src/main/java/com/dua3/utility/math/Vec2d.java
@@ -15,6 +15,8 @@ public final class Vec2d {
     private final double y;
 
     public static final Vec2d ORIGIN = new Vec2d(0,0);
+    public static final Vec2d POSITIVE_INFINITY = new Vec2d(Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
+    public static final Vec2d NEGATIVE_INFINITY = new Vec2d(Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY);
     
     private Vec2d(double x, double y) {
         this.x = x;
@@ -29,7 +31,58 @@ public final class Vec2d {
     public static Vec2d of(double x, double y) {
         return new Vec2d(x,y);
     }
-    
+
+    /**
+     * Return result vector of componentwise maximum.
+     * @param a first vector
+     * @param b second vector
+     * @return Vector with the the componentwise maxima of a and b as components. 
+     */
+    public static Vec2d max(Vec2d a, Vec2d b) {
+        return Vec2d.of(Math.max(a.x, b.x), Math.max(a.y, b.y));
+    }
+
+    /**
+     * Return result vector of componentwise minimum.
+     * @param a first vector
+     * @param b second vector
+     * @return Vector with the the componentwise minima of a and b as components. 
+     */
+    public static Vec2d min(Vec2d a, Vec2d b) {
+        return Vec2d.of(Math.min(a.x, b.x), Math.min(a.y, b.y));
+    }
+
+    /**
+     * Scalar product.
+     * @param a first vector
+     * @param b second vector
+     * @return this * v
+     */
+    public static double scalarProduct(Vec2d a, Vec2d b) {
+        return a.x*b.x+a.y*b.y;
+    }
+
+    /**
+     * Return angle in radians between two vectors.
+     * @param a first vector
+     * @param b second vector
+     * @return the agle in radians 
+     */
+    public static double angle(Vec2d a, Vec2d b) {
+        double nominator = scalarProduct(a, b);
+        
+        if (nominator==0) {
+            return Double.NaN;
+        }
+        
+        double denominator = a.length() * b.length();
+
+        // denominator==0 implies nominator==0, so this should only happen if scalarProdut(9 or length() is broken
+        assert denominator!= 0;  
+        
+        return Math.acos(nominator/denominator);
+    }
+
     /**
      * @return the x-coordinate.
      */
@@ -63,20 +116,11 @@ public final class Vec2d {
     }
     
     /**
-     * Scalar product.
-     * @param v the argument
-     * @return this * v
-     */
-    public double scalarProduct(Vec2d v) {
-        return x*v.x+y*v.y;    
-    }
-
-    /**
      * Calculate length of vector.
      * @return the length
      */
     public double length() {
-        return Math.sqrt(scalarProduct(this));
+        return Math.sqrt(scalarProduct(this, this));
     }
 
     @Override
