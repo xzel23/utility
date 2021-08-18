52949df50a16b4c4a71e5f2556696e818ea7da1c
Axel Howind <axel@dua3.com>
2021-08-18 16:18:58 +0200

    code cleanup

--- utility/src/main/java/com/dua3/utility/lang/NamedFunction.java
+++ utility/src/main/java/com/dua3/utility/lang/NamedFunction.java
@@ -13,33 +13,11 @@ import java.util.function.Function;
  * @param <R>
  *        type of function return value
  */
-public class NamedFunction<T, R> implements Function<T, R> {
+public record NamedFunction<T, R>(String name, Function<T, R> f) implements Function<T, R> {
 
-    /**
-     * Create a new named function.
-     *
-     * @param       <T>
-     *              type of funcion argument
-     * @param       <R>
-     *              type of function return value
-     * @param  name
-     *              the function name
-     * @param  f
-     *              the function
-     * @return
-     *              new NamedFunction instance
-     */
-    public static <T, R> NamedFunction<T, R> create(String name, Function<T, R> f) {
-        return new NamedFunction<>(name, f);
-    }
-
-    private final String name;
-
-    private final Function<T, R> f;
-
-    protected NamedFunction(String name, Function<T, R> f) {
-        this.name = Objects.requireNonNull(name);
-        this.f = Objects.requireNonNull(f);
+    public NamedFunction {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(f);
     }
 
     @Override
