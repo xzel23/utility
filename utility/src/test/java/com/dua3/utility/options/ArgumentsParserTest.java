693339a06c27e7c9aac64e0ebab873e4dcd71e8d
Axel Howind <axel@dua3.com>
2021-10-10 12:55:12 +0200

    rename test class to match renamed class

--- utility/src/test/java/com/dua3/utility/options/ArgumentsParserTest.java
+++ utility/src/test/java/com/dua3/utility/options/ArgumentsParserTest.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CmdTest {
+public class ArgumentsParserTest {
 
     @Test
     public void testFlag() {
