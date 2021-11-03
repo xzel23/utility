e2f3cd4efdc17eb64cd07cf8852476bcf53280f8
Axel Howind <axel@dua3.com>
2021-11-03 08:47:12 +0100

    add javadoc

--- utility/src/main/java/com/dua3/utility/math/Rectangle2f.java
+++ utility/src/main/java/com/dua3/utility/math/Rectangle2f.java
@@ -8,13 +8,22 @@ package com.dua3.utility.math;
 import com.dua3.utility.lang.LangUtil;
 import com.dua3.utility.math.geometry.Dimension2f;
 
+/**
+ * Immutable rectangle with float coordinates.
+ */
 public record Rectangle2f(float x, float y, float width, float height) {
 
     public Rectangle2f {
         assert width>=0;
         assert height>=0;
     }
-    
+
+    /**
+     * Create rectangle from two vectors.
+     * @param p the first corner
+     * @param q the second corner
+     * @return rectangle
+     */
     public static Rectangle2f withCorners(Vector2f p, Vector2f q) {
         float x,y,w,h;
         if (p.x()<=q.x()) {
@@ -34,27 +43,57 @@ public record Rectangle2f(float x, float y, float width, float height) {
         return new Rectangle2f(x,y,w,h);
     }
 
+    /**
+     * Create rectangle from coordinates and dimension. Both width and height must be non-negative.
+     * @param x the x-coordinate 
+     * @param y the y-coordinate
+     * @param w the width
+     * @param h the height
+     * @return rectangle
+     */
     public static Rectangle2f of(float x, float y, float w, float h) {
         LangUtil.check(w>=0 && h>=0, "w and h must not be negative: w=%f, h=%f", w, h);
         return new Rectangle2f(x, y, w, h);
     }
 
+    /**
+     * Create rectangle from corner and dimension.
+     * @param p the vector to the corner 
+     * @param d the rectangle dimension
+     * @return rectangle
+     */
     public static Rectangle2f of(Vector2f p, Dimension2f d) {
         return Rectangle2f.of(p.x(), p.y(), d.width(), d.height());    
-    } 
-    
+    }
+
+    /**
+     * The minimum of the two x-coordinates.
+     * @return minimum x-coordinate
+     */
     public float xMin() {
         return x;
     }
 
+    /**
+     * The minimum of the two y-coordinates.
+     * @return minimum y-coordinate
+     */
     public float yMin() {
         return y;
     }
 
+    /**
+     * The maximum of the two x-coordinates.
+     * @return maximum x-coordinate
+     */
     public float xMax() {
         return x+width;
     }
 
+    /**
+     * The maximum of the two y-coordinates.
+     * @return maximum y-coordinate
+     */
     public float yMax() {
         return y+height;
     }
