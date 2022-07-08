920e89c7fdc79416230bed8059045e3651c6c798
Axel Howind <axel@dua3.com>
2022-07-08 10:31:35 +0200

    v10.0.0-RC5

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 /////////////////////////////////////////////////////////////////////////////
 object meta {
     val group           = "com.dua3.utility"
-    val version         = "10.0.0-RC5-SNAPSHOT" 
+    val version         = "10.0.0-RC5" 
     val scm             = "https://gitlab.com/com.dua3/lib/utility.git"
     val repo            = "public"
     val licenseName     = "MIT"
