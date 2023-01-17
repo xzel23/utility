ae187f935d73fcdd5c80649df6d1ff4a222587c4
Axel Howind <axh@dua3.com>
2023-01-17 11:40:56 +0100

    update junit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
             plugin("cabe", "com.dua3.cabe").version("1.0.0")
 
             version("cabe", "1.0.0")
-            version("junit", "5.9.1")
+            version("junit", "5.9.2")
             version("log4j", "2.19.0")
             version("slf4j", "2.0.6")
 
