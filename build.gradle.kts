b0a6c5b5628c98518c89d233a8da15ff105bd23a
Axel Howind <axh@dua3.com>
2023-02-24 09:37:59 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import java.net.URI
 import com.adarshr.gradle.testlogger.theme.ThemeType
+import java.net.URI
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
