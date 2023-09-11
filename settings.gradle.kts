88cfb89f9a77dcf6923bf1d940b35ffb53305a38
Axel Howind <axh@dua3.com>
2023-09-11 07:41:28 +0200

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.47.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.48.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
             plugin("spotbugs", "com.github.spotbugs").version("5.1.3")
             plugin("cabe", "com.dua3.cabe").version("1.3.0")
@@ -23,7 +23,7 @@ dependencyResolutionManagement {
             version("cabe", "1.0.0")
             version("junit", "5.10.0")
             version("log4j", "2.20.0")
-            version("slf4j", "2.0.7")
+            version("slf4j", "2.0.9")
             version("jimfs", "1.2")
 
             library("cabe-annotations", "com.dua3.cabe", "cabe-annotations").versionRef("cabe")
