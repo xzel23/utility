6dccf731e1c845be7b9025b0041362b3ee82a9b5
Axel Howind <axh@dua3.com>
2023-11-19 09:10:27 +0100

    update versions plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.49.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.50.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("spotbugs", "com.github.spotbugs").version("6.0.0-rc.2")
             plugin("cabe", "com.dua3.cabe").version("1.3.0")
