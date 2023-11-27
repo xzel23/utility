e6b7c7e1d6221fd2018d69ac42224468f4b9ac5b
Axel Howind <axh@dua3.com>
2023-11-27 07:49:59 +0100

    renamed test class

--- utility/src/test/java/com/dua3/utility/text/AnsiCodeTest.java
+++ utility/src/test/java/com/dua3/utility/text/AnsiCodeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestAnsiCode {
+public class AnsiCodeTest {
 
     @Test
     public void testStyles() {
