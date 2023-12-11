80f369abdded091e2ef26d5b818797a4af8246af
Axel Howind <axh@dua3.com>
2023-12-11 22:50:38 +0100

    add unit tests

---
+++ utility/src/test/java/com/dua3/utility/data/HSVColorTest.java
@@ -0,0 +1,36 @@
+package com.dua3.utility.data;
+
+import com.dua3.utility.data.HSVColor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * `HSVColorTest` is used to test the functionality of `HSVColor` class.
+ * Specifically, this test focuses on the `valueOf` method, which converts an ARGB integer to `HSVColor`.
+ */
+public class HSVColorTest {
+
+    /**
+     * This test providing a ARGB color value and validating if returned `HSVColor` instance has the correct values.
+     */
+    @Test
+    void valueOfTest() {
+        // RGB: 169, 169, 169 (grey)
+        // HSV: 0, 0, 0.6627
+        int argb = 0xffa9a9a9;
+
+        HSVColor hsvColor = HSVColor.valueOf(argb);
+
+        // Hue should be zero for grey scale color
+        Assertions.assertEquals(0.0, hsvColor.h(), "Hue doesn't match with the expected value");
+
+        // Saturation should be zero for grey scale color
+        Assertions.assertEquals(0.0, hsvColor.s(), "Saturation doesn't match with the expected value");
+
+        // Value or brightness should be ~0.66 for above RGB values
+        Assertions.assertEquals(0.6627, hsvColor.v(), 0.01, "Brightness doesn't match with the expected value");
+
+        // Alpha should be ~1 for above ARGB value where alpha component is 255
+        Assertions.assertEquals(1, hsvColor.alpha(), "Alpha doesn't match with the expected value");
+    }
+}
---
+++ utility/src/test/java/com/dua3/utility/data/MappingIteratorTest.java
@@ -0,0 +1,48 @@
+package com.dua3.utility.data;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class MappingIteratorTest {
+    @Test
+    void testNext() {
+        // setup
+        Iterator<Integer> integers = Arrays.asList(1, 2, 3).iterator();
+        Function<Integer, Integer> doubling = i -> i * 2;
+        MappingIterator<Integer, Integer> mappingIterator = new MappingIterator<>(integers, doubling);
+
+        // execute & validate
+        assertEquals(2, (int) mappingIterator.next());
+        assertEquals(4, (int) mappingIterator.next());
+        assertEquals(6, (int) mappingIterator.next());
+    }
+
+    @Test
+    void testHasNextWithElements() {
+        // setup
+        Iterator<String> strings = Arrays.asList("A", "B", "C").iterator();
+        Function<String, String> repeat = s -> s.repeat(2);
+        MappingIterator<String, String> mappingIterator = new MappingIterator<>(strings, repeat);
+
+        // execute & validate
+        assertEquals(true, mappingIterator.hasNext());
+    }
+
+    @Test
+    void testHasNextWithoutElements() {
+        // setup
+        Iterator<String> emptyIterator = List.<String>of().iterator();
+        Function<String, String> repeat = s -> s.repeat(2);
+        MappingIterator<String, String> mappingIterator = new MappingIterator<>(emptyIterator, repeat);
+
+        // execute & validate
+        assertFalse(mappingIterator.hasNext());
+    }
+}
