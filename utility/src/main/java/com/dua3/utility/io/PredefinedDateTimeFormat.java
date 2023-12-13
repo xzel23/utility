137bf5a748230f9af32b56e4f88d8e83ed8e8b57
Axel Howind <axh@dua3.com>
2023-12-13 20:36:27 +0100

    add tests

--- utility/src/main/java/com/dua3/utility/io/PredefinedDateTimeFormat.java
+++ utility/src/main/java/com/dua3/utility/io/PredefinedDateTimeFormat.java
@@ -1,11 +1,16 @@
 package com.dua3.utility.io;
 
+import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 public enum PredefinedDateTimeFormat {
+    LOCALE_DEFAULT("locale dependent", PredefinedDateTimeFormat::formatDateTimeFromLocale, PredefinedDateTimeFormat::formatDateFromLocale, PredefinedDateTimeFormat::formatTimeFromLocale),
+
     /**
      * Represents a locale-dependent short format style.
      * <p>
@@ -45,27 +50,7 @@ public enum PredefinedDateTimeFormat {
      * @see FormatStyle#MEDIUM
      * @see java.time.LocalDate#format(DateTimeFormatter)
      */
-    LOCALE_MEDIUM("short (locale dependent)", FormatStyle.MEDIUM),
-    /**
-     * Represents a locale-dependent long format style.
-     * <p>
-     * The LOCALE_LONG constant can be used to format dates and times in a long format, using the
-     * locale-specific conventions.
-     * <p>
-     * The format style used is determined based on the locale set in the system. The format style
-     * defines the format pattern used to represent the long date and time format.
-     * <p>
-     * Example usage:
-     * <pre>
-     *     LocalDate date = LocalDate.now();
-     *     String formattedDate = date.format(LOCALE_LONG.getDateTimeFormatter());
-     *     System.out.println(formattedDate);
-     * </pre>
-     *
-     * @see FormatStyle#LONG
-     * @see java.time.LocalDate#format(DateTimeFormatter)
-     */
-    LOCALE_LONG("long (locale dependent)", FormatStyle.LONG),
+    LOCALE_MEDIUM("medium (locale dependent)", FormatStyle.MEDIUM),
     /**
      * Represents the format for ISO 8601 formatted date and time.
      * <p>
@@ -81,7 +66,9 @@ public enum PredefinedDateTimeFormat {
      *
      * @see java.time.LocalDate#format(DateTimeFormatter)
      */
-    ISO_DATE_TIME("ISO 8601 (2000-12-31T10:15:30)", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    ISO_DATE_TIME("ISO 8601 (2000-12-31T10:15:30)", locale -> DateTimeFormatter.ISO_LOCAL_DATE_TIME, locale -> DateTimeFormatter.ISO_LOCAL_DATE, locale -> DateTimeFormatter.ISO_LOCAL_TIME);
+
+    static final Pattern PATTERN_YEAR_PATTERN = Pattern.compile("\\byy\\b");
 
     private final String name;
 
@@ -89,11 +76,11 @@ public enum PredefinedDateTimeFormat {
     private final Function<? super Locale, DateTimeFormatter> dateFormatterFactory;
     private final Function<? super Locale, DateTimeFormatter> timeFormatterFactory;
 
-    PredefinedDateTimeFormat(String name, DateTimeFormatter formatter) {
+    PredefinedDateTimeFormat(String name, Function<? super Locale, DateTimeFormatter> dateTimeFormatterFactory, Function<? super Locale, DateTimeFormatter> dateFormatterFactory, Function<? super Locale, DateTimeFormatter> timeFormatterFactory) {
         this.name = name;
-        this.dateTimeFormatterFactory = locale -> formatter;
-        this.dateFormatterFactory = locale -> formatter;
-        this.timeFormatterFactory = locale -> formatter;
+        this.dateTimeFormatterFactory = dateTimeFormatterFactory;
+        this.dateFormatterFactory = dateFormatterFactory;
+        this.timeFormatterFactory = timeFormatterFactory;
     }
 
     PredefinedDateTimeFormat(String name, FormatStyle style) {
@@ -101,6 +88,57 @@ public enum PredefinedDateTimeFormat {
         this.dateTimeFormatterFactory = locale -> DateTimeFormatter.ofLocalizedDateTime(style).withLocale(locale);
         this.dateFormatterFactory = locale -> DateTimeFormatter.ofLocalizedDate(style).withLocale(locale);
         this.timeFormatterFactory = locale -> DateTimeFormatter.ofLocalizedTime(style).withLocale(locale);
+    }
+
+    /**
+     * Create a locale dependent DateTimeFormatter for a short date time format with a four-digit year.
+     *
+     * @param locale the locale
+     * @return the DateTimeFormatter
+     */
+    private static DateTimeFormatter formatDateTimeFromLocale(Locale locale) {
+        String formatPattern =
+                DateTimeFormatterBuilder.getLocalizedDateTimePattern(
+                        FormatStyle.SHORT,
+                        FormatStyle.MEDIUM,
+                        IsoChronology.INSTANCE,
+                        locale);
+        formatPattern = PATTERN_YEAR_PATTERN.matcher(formatPattern).replaceAll("yyyy");
+        return DateTimeFormatter.ofPattern(formatPattern, locale);
+    }
+
+    /**
+     * Create a locale dependent DateTimeFormatter for a short date format with a four-digit year.
+     *
+     * @param locale the locale
+     * @return the DateFormatter
+     */
+    private static DateTimeFormatter formatDateFromLocale(Locale locale) {
+        String formatPattern =
+                DateTimeFormatterBuilder.getLocalizedDateTimePattern(
+                        FormatStyle.SHORT,
+                        null,
+                        IsoChronology.INSTANCE,
+                        locale);
+        formatPattern = PATTERN_YEAR_PATTERN.matcher(formatPattern).replaceAll("yyyy");
+        return DateTimeFormatter.ofPattern(formatPattern, locale);
+    }
+
+    /**
+     * Create a locale dependent DateTimeFormatter for a time format.
+     *
+     * @param locale the locale
+     * @return the DateFormatter
+     */
+    private static DateTimeFormatter formatTimeFromLocale(Locale locale) {
+        String formatPattern =
+                DateTimeFormatterBuilder.getLocalizedDateTimePattern(
+                        null,
+                        FormatStyle.SHORT,
+                        IsoChronology.INSTANCE,
+                        locale);
+        formatPattern = PATTERN_YEAR_PATTERN.matcher(formatPattern).replaceAll("yyyy");
+        return DateTimeFormatter.ofPattern(formatPattern, locale);
     }
 
     /**
---
+++ utility/src/test/java/com/dua3/utility/io/PredefinedDateTimeFormatTest.java
@@ -0,0 +1,123 @@
+package com.dua3.utility.io;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PredefinedDateTimeFormatTest {
+
+    private static Stream<Arguments> provideParametersForDateTimeFormatTesting() {
+        return Stream.of(
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.ROOT, LocalDateTime.of(2001, 1, 1, 2, 30), "2001-01-01 02:30:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.US, LocalDateTime.of(2001, 1, 1, 3, 45), "1/1/2001, 3:45:00 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.GERMANY, LocalDateTime.of(2001, 1, 1, 12, 15), "01.01.2001, 12:15:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.FRANCE, LocalDateTime.of(2001, 1, 1, 23, 59), "01/01/2001 23:59:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.CHINA, LocalDateTime.of(2001, 1, 1, 18, 22), "2001/1/1 下午6:22:00"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.US, LocalDateTime.of(2021, 12, 31, 10, 10), "12/31/21, 10:10 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.GERMANY, LocalDateTime.of(2021, 12, 31, 6, 6), "31.12.21, 06:06"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.FRANCE, LocalDateTime.of(2021, 12, 31, 19, 35), "31/12/2021 19:35"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.CHINA, LocalDateTime.of(2021, 12, 31, 9, 30), "2021/12/31 上午9:30"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.US, LocalDateTime.of(2021, 12, 31, 10, 10), "Dec 31, 2021, 10:10:00 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.GERMANY, LocalDateTime.of(2021, 12, 31, 6, 6), "31.12.2021, 06:06:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.FRANCE, LocalDateTime.of(2021, 12, 31, 19, 35), "31 déc. 2021, 19:35:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.CHINA, LocalDateTime.of(2021, 12, 31, 9, 30), "2021年12月31日 上午9:30:00"),
+
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.US, LocalDateTime.of(2021, 12, 31, 10, 43), "2021-12-31T10:43:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.GERMANY, LocalDateTime.of(2021, 12, 31, 13, 58), "2021-12-31T13:58:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.FRANCE, LocalDateTime.of(2021, 12, 31, 4, 21), "2021-12-31T04:21:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.CHINA, LocalDateTime.of(2021, 12, 31, 22, 0), "2021-12-31T22:00:00")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParametersForDateTimeFormatTesting")
+    void testDateTimeFormats(PredefinedDateTimeFormat format, Locale locale, LocalDateTime date, String expectedFormat) {
+        // Prepare expected DateTimeFormatter
+        DateTimeFormatter formatter = format.getDateTimeFormatter(locale);
+
+        // Validate the date formatting
+        assertEquals(expectedFormat, formatter.format(date), format.getClass().getSimpleName()+"."+format.name()+"@"+locale);
+    }
+
+    private static Stream<Arguments> provideParametersForDateFormatTesting() {
+        return Stream.of(
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.ROOT, LocalDate.of(2001, 1, 1), "2001-01-01"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.US, LocalDate.of(2001, 1, 1), "1/1/2001"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.GERMANY, LocalDate.of(2001, 1, 1), "01.01.2001"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.FRANCE, LocalDate.of(2001, 1, 1), "01/01/2001"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.CHINA, LocalDate.of(2001, 1, 1), "2001/1/1"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.US, LocalDate.of(2021, 12, 31), "12/31/21"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.GERMANY, LocalDate.of(2021, 12, 31), "31.12.21"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.FRANCE, LocalDate.of(2021, 12, 31), "31/12/2021"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.CHINA, LocalDate.of(2021, 12, 31), "2021/12/31"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.US, LocalDate.of(2021, 12, 31), "Dec 31, 2021"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.GERMANY, LocalDate.of(2021, 12, 31), "31.12.2021"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.FRANCE, LocalDate.of(2021, 12, 31), "31 déc. 2021"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.CHINA, LocalDate.of(2021, 12, 31), "2021年12月31日"),
+
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.US, LocalDate.of(2021, 12, 31), "2021-12-31"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.GERMANY, LocalDate.of(2021, 12, 31), "2021-12-31"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.FRANCE, LocalDate.of(2021, 12, 31), "2021-12-31"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.CHINA, LocalDate.of(2021, 12, 31), "2021-12-31")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParametersForDateFormatTesting")
+    void testDateFormats(PredefinedDateTimeFormat format, Locale locale, LocalDate date, String expectedFormat) {
+        // Prepare expected DateTimeFormatter
+        DateTimeFormatter formatter = format.getDateFormatter(locale);
+
+        // Validate the date formatting
+        assertEquals(expectedFormat, formatter.format(date), format.name());
+    }
+
+    private static Stream<Arguments> provideParametersForTimeFormatTesting() {
+        return Stream.of(
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.ROOT, LocalTime.of(2, 30), "02:30"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.US, LocalTime.of(3, 45), "3:45 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.GERMANY, LocalTime.of(12, 15), "12:15"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.FRANCE, LocalTime.of(23, 59), "23:59"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_DEFAULT, Locale.CHINA, LocalTime.of(18, 22), "下午6:22"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.US, LocalTime.of(10, 10), "10:10 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.GERMANY, LocalTime.of(6, 6), "06:06"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.FRANCE, LocalTime.of(19, 35), "19:35"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_SHORT, Locale.CHINA, LocalTime.of(9, 30), "上午9:30"),
+
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.US, LocalTime.of(10, 10), "10:10:00 AM"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.GERMANY, LocalTime.of(6, 6), "06:06:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.FRANCE, LocalTime.of(19, 35), "19:35:00"),
+                Arguments.of(PredefinedDateTimeFormat.LOCALE_MEDIUM, Locale.CHINA, LocalTime.of(9, 30), "上午9:30:00"),
+
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.US, LocalTime.of(10, 43), "10:43:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.GERMANY, LocalTime.of(13, 58), "13:58:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.FRANCE, LocalTime.of(4, 21), "04:21:00"),
+                Arguments.of(PredefinedDateTimeFormat.ISO_DATE_TIME, Locale.CHINA, LocalTime.of(22, 0), "22:00:00")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParametersForTimeFormatTesting")
+    void testTimeFormats(PredefinedDateTimeFormat format, Locale locale, LocalTime time, String expectedFormat) {
+        // Prepare expected DateTimeFormatter
+        DateTimeFormatter formatter = format.getTimeFormatter(locale);
+
+        // Validate the date formatting
+        assertEquals(expectedFormat, formatter.format(time), format.getClass().getSimpleName()+"."+format.name()+"@"+locale);
+    }
+
+}
