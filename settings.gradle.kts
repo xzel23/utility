8dbcbba96376f6e590507031602793c7d9ec66fd
Axel Howind <axh@dua3.com>
2023-12-16 06:09:57 +0100

    v12.0.0-beta9; update spotbugs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "12.0.0-beta8"
+val projectVersion = "12.0.0-beta9"
 
 // define subprojects
 include("utility")
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.50.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.2")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.4")
             plugin("cabe", "com.dua3.cabe").version("1.3.0")
 
             version("cabe", "1.0.0")
