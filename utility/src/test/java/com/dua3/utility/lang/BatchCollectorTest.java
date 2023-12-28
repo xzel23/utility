f7cae0f7bb240e7da665fe2b377113eb4255e4c3
Axel Howind <axh@dua3.com>
2023-12-28 13:23:03 +0100

    code cleanup

--- utility/src/test/java/com/dua3/utility/lang/BatchCollectorTest.java
+++ utility/src/test/java/com/dua3/utility/lang/BatchCollectorTest.java
@@ -1,12 +1,9 @@
 package com.dua3.utility.lang;
 
-import com.dua3.utility.data.Pair;
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
+
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
