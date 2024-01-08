a18f831181d82f79f1d1f71b78a8ea41cb98d51e
Axel Howind <axh@dua3.com>
2024-01-08 22:50:29 +0100

    add javadoc

--- utility/src/main/java/com/dua3/utility/math/geometry/Arc2f.java
+++ utility/src/main/java/com/dua3/utility/math/geometry/Arc2f.java
@@ -1,12 +1,9 @@
 package com.dua3.utility.math.geometry;
 
 /**
- * Arc Path segment.
+ * Represents a 2-dimensional arc curve.
  */
 public final class Arc2f extends AbstractCurve2f {
-    /**
-     * Name of this segment type.
-     */
     public static final String NAME = "ARC";
 
     private float rx;
@@ -15,6 +12,18 @@ public final class Arc2f extends AbstractCurve2f {
     private boolean largeArc;
     private boolean sweep;
 
+    /**
+     * Represents a 2-dimensional arc curve.
+     *
+     * @param path     the path this curve belongs to
+     * @param b0       the index of the starting vertex
+     * @param b1       the index of the ending vertex
+     * @param rx       the x-radius of the arc
+     * @param ry       the y-radius of the arc
+     * @param angle    the angle of the arc in radians
+     * @param largeArc true if the arc is greater than 180 degrees, false otherwise
+     * @param sweep    true if the arc sweeps in a positive angle direction, false otherwise
+     */
     Arc2f(Path2fImpl path, int b0, int b1, float rx, float ry, float angle, boolean largeArc, boolean sweep) {
         super(path, b0, b1);
         this.rx = rx;
@@ -29,22 +38,47 @@ public final class Arc2f extends AbstractCurve2f {
         return NAME;
     }
 
+    /**
+     * Returns the x-radius of the arc curve.
+     *
+     * @return the x-radius of the arc curve
+     */
     public float rx() {
         return rx;
     }
 
+    /**
+     * Returns the y-radius of the arc curve.
+     *
+     * @return the y-radius of the arc curve
+     */
     public float ry() {
         return ry;
     }
 
+    /**
+     * Returns the angle of the arc curve in radians.
+     *
+     * @return the angle of the arc curve in radians
+     */
     public float angle() {
         return angle;
     }
 
+    /**
+     * Returns the value of the largeArc property for the Arc2f object.
+     *
+     * @return true if the arc is greater than 180 degrees, false otherwise
+     */
     public boolean largeArc() {
         return largeArc;
     }
 
+    /**
+     * Returns the value of the sweep property for the Arc2f object.
+     *
+     * @return true if the arc sweeps in a positive angle direction, false otherwise
+     */
     public boolean sweep() {
         return sweep;
     }
