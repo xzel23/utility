c0de1af1811a59f20e0f93c0570f9c6ec606e800
Axel Howind <axh@dua3.com>
2024-01-22 11:23:31 +0100

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "12.0.3"
+val projectVersion = "12.0.4-SNAPSHOT"
 
 // define subprojects
 include("utility")
