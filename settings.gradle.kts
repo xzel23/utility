8b3961943557435c809c92862a5fa020bda6a67f
Axel Howind <axh@dua3.com>
2024-01-22 11:23:11 +0100

    update cabe

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
             plugin("versions", "com.github.ben-manes.versions").version("0.50.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("spotbugs", "com.github.spotbugs").version("6.0.6")
-            plugin("cabe", "com.dua3.cabe").version("2.1-rc1")
+            plugin("cabe", "com.dua3.cabe").version("2.1-rc2")
 
             version("cabe-annotations", "2.0")
             version("junit", "5.10.1")
