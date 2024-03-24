77856c9b13b365fa237ec77a12e58dc3691d4360
Axel Howind <axh@dua3.com>
2024-03-24 05:17:02 +0100

    v12.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "12.0.10-SNAPSHOT"
+val projectVersion = "12.1.0"
 
 // define subprojects
 include("utility")
