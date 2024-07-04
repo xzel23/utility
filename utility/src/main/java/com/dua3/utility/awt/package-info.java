4f031ca31bc3a2b12f7be21fea8aa6bb4b95d986
Axel Howind <axh@dua3.com>
2024-07-04 21:57:24 +0200

    add missing package info files

---
+++ utility/src/main/java/com/dua3/utility/awt/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains awt related classes.
+ */
+@NotNullApi
+package com.dua3.utility.awt;
+
+import com.dua3.cabe.annotations.NotNullApi;
---
+++ utility/src/main/java/com/dua3/utility/i18n/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes for international applications.
+ */
+@NotNullApi
+package com.dua3.utility.i18n;
+
+import com.dua3.cabe.annotations.NotNullApi;
