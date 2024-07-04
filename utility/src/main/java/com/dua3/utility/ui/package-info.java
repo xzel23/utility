c9dfdf63c042f4dab4aaf27feb5578f751a62291
Axel Howind <axh@dua3.com>
2024-07-04 21:59:05 +0200

    add ui module with Graphics interface and Swing and JavaFX implementations

---
+++ utility/src/main/java/com/dua3/utility/ui/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains user interface related classes.
+ */
+@NotNullApi
+package com.dua3.utility.ui;
+
+import com.dua3.cabe.annotations.NotNullApi;
--- utility/src/main/java/module-info.java
+++ utility/src/main/java/module-info.java
@@ -50,6 +50,7 @@ open module com.dua3.utility {
     exports com.dua3.utility.options;
     exports com.dua3.utility.text;
     exports com.dua3.utility.spi;
+    exports com.dua3.utility.ui;
     exports com.dua3.utility.xml;
     exports com.dua3.utility.awt;
 
