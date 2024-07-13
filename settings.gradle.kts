33cb77baf787c8aad84787b871b3f81c8dbcd017
Axel Howind <axh@dua3.com>
2024-07-13 17:10:27 +0200

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.51.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.18")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.19")
             plugin("cabe", "com.dua3.cabe").version("2.1.2")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
@@ -45,7 +45,7 @@ dependencyResolutionManagement {
             version("slf4j", "2.0.13")
             version("ikonli", "12.3.1")
             version("jimfs", "1.3.0")
-            version("miglayout", "11.3")
+            version("miglayout", "11.4")
             version("mockito", "5.12.0")
             version("javafx", "22")
 
