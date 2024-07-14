cb19331fdafdafbd949e7f9f2e1dfd8785da6c27
Axel Howind <axh@dua3.com>
2024-07-14 12:45:33 +0200

    improve output for failed test

---
+++ utility/src/test/java/com/dua3/utility/text/TextAssertions.java
@@ -0,0 +1,15 @@
+package com.dua3.utility.text;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TextAssertions {
+    public static void assertEqualsWithEscapedOutput(String expected, String actual, String name) {
+        if (!expected.equals(actual)) {
+            // Escape the whitespace characters
+            String escapedExpected = TextUtil.escape(expected);
+            String escapedActual = TextUtil.escape(actual);
+            // Fail the test with detailed message
+            fail("[" + name + "]Expected:\n" + escapedExpected + "\nActual:\n" + escapedActual);
+        }
+    }
+}
