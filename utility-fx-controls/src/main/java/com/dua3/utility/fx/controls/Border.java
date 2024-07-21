ca133d1428f8c017e15aa039284c013558dc2dd0
Axel Howind <axh@dua3.com>
2024-07-21 22:59:32 +0200

    rename Border enum to Position

--- utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/Border.java
+++
@@ -1,8 +0,0 @@
-package com.dua3.utility.fx.controls;
-
-/**
- * Enum declaring values for the four borders.
- */
-public enum Border {
-    TOP, BOTTOM, LEFT, RIGHT
-}
---
+++ utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/Position.java
@@ -0,0 +1,15 @@
+package com.dua3.utility.fx.controls;
+
+/**
+ * Enumeration representing the possible positions of borders and edges.
+ */
+public enum Position {
+    /** The top position. */
+    TOP,
+    /** The bottom position. */
+    BOTTOM,
+    /** The left position. */
+    LEFT,
+    /** The right position. */
+    RIGHT
+}
