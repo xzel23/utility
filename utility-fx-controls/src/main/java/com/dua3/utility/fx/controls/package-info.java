6c3f39fb481829d027051d561b8e74c9e0b48cb2
Axel Howind <axh@dua3.com>
2024-11-02 03:27:39 +0100

    remove unused imports

--- utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/package-info.java
+++ utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/package-info.java
@@ -2,5 +2,3 @@
  * This package contains custom JavaFX controls.
  */
 package com.dua3.utility.fx.controls;
-
-import org.jspecify.annotations.NullMarked;
--- utility-fx-icons/src/main/java/com/dua3/utility/fx/icons/package-info.java
+++ utility-fx-icons/src/main/java/com/dua3/utility/fx/icons/package-info.java
@@ -2,5 +2,3 @@
  * This package contains classes for working with icons in JavaFX.
  */
 package com.dua3.utility.fx.icons;
-
-import org.jspecify.annotations.NullMarked;
