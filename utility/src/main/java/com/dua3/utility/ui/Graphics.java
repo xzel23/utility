f27268195ca2ccd9bdf0c1a38be9ba6769cd4b2c
Axel Howind <axh@dua3.com>
2025-01-25 11:43:18 +0100

    move FragmentedText to top level

--- utility/src/main/java/com/dua3/utility/ui/Graphics.java
+++ utility/src/main/java/com/dua3/utility/ui/Graphics.java
@@ -13,16 +13,11 @@ import com.dua3.utility.text.Alignment;
 import com.dua3.utility.text.Font;
 import com.dua3.utility.text.FontUtil;
 import com.dua3.utility.text.RichText;
-import com.dua3.utility.text.Run;
-import com.dua3.utility.text.TextUtil;
 import com.dua3.utility.text.VerticalAlignment;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 /**
  * A generic interface defining drawing commands.
@@ -526,7 +521,7 @@ public interface Graphics extends AutoCloseable {
         float width = outputDimension.width();
         float height = outputDimension.height();
 
-        FragmentedText fragments = generateFragments(text, width, height, hAlign, vAlign, hAnchor, vAnchor);
+        FragmentedText fragments = FragmentedText.generateFragments(text, getFontUtil(), getFont(), width, height, hAlign, vAlign, hAnchor, vAnchor);
 
         renderFragments(
                 pos,
@@ -572,7 +567,7 @@ public interface Graphics extends AutoCloseable {
         float height = outputDimension.height();
 
         // layout text
-        FragmentedText fragments = generateFragments(text, width, height, hAlign, vAlign, hAnchor, vAnchor);
+        FragmentedText fragments = FragmentedText.generateFragments(text, getFontUtil(), getFont(), width, height, hAlign, vAlign, hAnchor, vAnchor);
 
         switch (mode) {
             case ROTATE_BLOCK -> {
@@ -594,24 +589,24 @@ public interface Graphics extends AutoCloseable {
 
                 switch (quadrant) {
                     case 0 -> {
-                        tx = (float) (Math.sin(angle) * fragments.textHeight());
+                        tx = (float) (Math.sin(angle) * fragments.height());
                         ty = 0;
                     }
                     case 1 -> {
-                        tx = (float) (Math.sin(angle) * fragments.textHeight()
-                                - Math.cos(angle) * fragments.textWidth()
+                        tx = (float) (Math.sin(angle) * fragments.height()
+                                - Math.cos(angle) * fragments.width()
                         );
-                        ty = (float) (-Math.cos(angle) * fragments.textHeight());
+                        ty = (float) (-Math.cos(angle) * fragments.height());
                     }
                     case 2 -> {
-                        tx = (float) (-Math.cos(angle) * fragments.textWidth());
-                        ty = (float) (-Math.sin(angle) * fragments.textWidth()
-                                - Math.cos(angle) * fragments.textHeight()
+                        tx = (float) (-Math.cos(angle) * fragments.width());
+                        ty = (float) (-Math.sin(angle) * fragments.width()
+                                - Math.cos(angle) * fragments.height()
                         );
                     }
                     case 3 -> {
                         tx = 0;
-                        ty = (float) (-Math.sin(angle) * fragments.textWidth());
+                        ty = (float) (-Math.sin(angle) * fragments.width());
                     }
                     default -> {
                         throw new IllegalStateException("invalid quadrant index: " + quadrant);
@@ -649,311 +644,18 @@ public interface Graphics extends AutoCloseable {
         return Vector2f.of(
                 switch (hAnchor) {
                     case LEFT -> 0;
-                    case RIGHT -> text.textWidth();
-                    case CENTER -> text.textWidth() / 2;
+                    case RIGHT -> text.width();
+                    case CENTER -> text.width() / 2;
                 },
                 switch (vAnchor) {
                     case TOP -> 0;
-                    case BOTTOM -> -text.textHeight();
-                    case MIDDLE -> -text.textHeight() / 2;
-                    case BASELINE -> -text.textHeight() + text.baseLine();
+                    case BOTTOM -> -text.height();
+                    case MIDDLE -> -text.height() / 2;
+                    case BASELINE -> -text.height() + text.baseLine();
                 }
         );
     }
 
-    /**
-     * A text fragment that can't be split (i.e., contains no whitespace) and has a uniform font so that it can be
-     * drawn in a single operation.
-     *
-     * @param x the x-position
-     * @param y the y-position
-     * @param w the width
-     * @param h the height
-     * @param baseLine the baseline value (of the line the fragment belongs to
-     * @param font the font
-     * @param text the text
-     */
-    record Fragment(float x, float y, float w, float h, float baseLine, Font font, CharSequence text) {
-        Fragment translate(float dx, float dy) {
-            return new Fragment(x + dx, y + dy, w, h, baseLine, font, text);
-        }
-    }
-
-    /**
-     * Represents a fragmented text that can be rendered within a specified bounding rectangle.
-     * <p>
-     * The FragmentedText class holds a list of lines, where each line is represented by a list of {@link Fragment}
-     * objects. Fragments are text segments that cannot be split and have uniform font attributes. The text is split
-     * based on whitespace characters and text decorations (font, text decoration). The class also stores the dimensions
-     * and position of the text within the bounding rectangle.
-     *
-     * @param lines a list of lines, where each line is represented by a list of Fragment objects
-     * @param textWidth the width of the rendered text
-     * @param textHeight the height of the rendered text
-     * @param baseLine the baseline value of the line the fragment belongs to
-     */
-    record FragmentedText(
-            List<List<Fragment>> lines,
-            float textWidth,
-            float textHeight,
-            float baseLine,
-            float actualWidth,
-            float actualHeight) {
-        /**
-         * Retrieves the layout dimensions of the text as a {@code Dimension2f} object.
-         *
-         * <p>>The dimensions are the ones received as input for the layout operation.
-         *
-         * @return a {@code Dimension2f} object representing the width and height of the text
-         */
-        public Dimension2f getLayoutDimension() {
-            return Dimension2f.of(textWidth, textHeight);
-        }
-
-        /**
-         * Retrieves the actual dimensions of the text as a {@code Dimension2f} object.
-         *
-         * <p>The dimensions are the calculated values after laying out the text.
-         *
-         * @return a {@code Dimension2f} object representing the width and height of the text
-         */
-        public Dimension2f getActualDimension() {
-            return Dimension2f.of(actualWidth, actualHeight);
-        }
-    }
-
-    /**
-     * Split text into fragments and layout according to alignment settings.
-     *
-     * <p>Split the text into fragments that are either whitespace or free of whitespace and have uniform
-     * text attributes (font, text decoration). For each line, a list of such fragments is generated and added
-     * to the list of fragment lines (see {@link FragmentedText#lines()}).
-     *
-     * <p><b>The layout algorithm</b>
-     * <ul>
-     * <li>For each line of text, automatic wrapping is applied when the line width exceeds {@code width}.
-     * <li>The fragments of each line are horizontally distributed according to the {@code hAlign} parameter.
-     * <li>The lines of fragments are then distributed vertically according to {@code vAlign} and {@code height}. If the
-     * given height is smaller than the required height, the text overflows.
-     * <li>The anchor is applied so that when the text is rendered, it is displayed according to the anchor setting.
-     * </ul>
-     *
-     * @param text              the text
-     * @param width             the width at which to apply wrapping; pass NO_WRAP to disable wrapping
-     * @param height            the height of the output area; used for vertical alignment
-     * @param hAlign            the horizontal alignment setting
-     * @param vAlign            the vertical alignment setting
-     * @param hAnchor           the horizontal anchor setting
-     * @param vAnchor           the vertical anchor setting
-     * @return the fragmented text as a {@link FragmentedText} instance
-     */
-    private FragmentedText generateFragments(
-            RichText text,
-            float width,
-            float height,
-            Alignment hAlign,
-            VerticalAlignment vAlign,
-            HAnchor hAnchor,
-            VAnchor vAnchor
-    ) {
-        boolean wrap = width != Float.MAX_VALUE;
-
-        Function<RichText, RichText> trimLine = switch (hAlign) {
-            case LEFT -> RichText::stripTrailing;
-            case RIGHT -> RichText::stripLeading;
-            case CENTER, JUSTIFY -> RichText::strip;
-        };
-
-        // generate lists of chunks for each line
-        FontUtil<?> fontUtil = getFontUtil();
-        List<List<Fragment>> fragmentLines = new ArrayList<>();
-        float textWidth = 0.0f;
-        float textHeight = 0.0f;
-        float baseLine = 0.0f;
-        for (RichText line : text.split("\n")) {
-            line = trimLine.apply(line);
-
-            List<Fragment> fragments = new ArrayList<>();
-            fragmentLines.add(fragments);
-
-            float xAct = 0.0f;
-            float lineHeight = 0.0f;
-            float lineWidth = 0.0f;
-            float lineBaseLine = 0.0f;
-            float whitespace = 0.0f;
-            boolean wrapAllowed = false;
-            List<Run> parts = splitLinePreservingWhitespace(line, wrap);
-            for (int i = 0; i < parts.size(); i++) {
-                var run = parts.get(i);
-
-                // when using JUSTIFY alignment, the last line
-                boolean isLastLine = i == parts.size() - 1;
-                Alignment effectiveHAlign = hAlign == Alignment.JUSTIFY && isLastLine ? Alignment.LEFT : hAlign;
-
-                Font f = fontUtil.deriveFont(getFont(), run.getFontDef());
-                Rectangle2f tr = fontUtil.getTextDimension(run, f);
-                if (wrapAllowed && xAct + tr.width() > width) {
-                    if (!fragments.isEmpty() && TextUtil.isBlank(fragments.get(fragments.size() - 1).text())) {
-                        // remove trailing whitespace
-                        Fragment removed = fragments.remove(fragments.size() - 1);
-                        whitespace -= removed.w();
-                        assert whitespace >= 0.0f : "whitespace must be non-negative after removing trailing whitespace";
-                    } else if (TextUtil.isBlank(run)) {
-                        // skip leading whitespace after wrapped line
-                        continue;
-                    }
-                    lineWidth = applyHAlign(fragments, hAlign, width, lineWidth, whitespace);
-                    textWidth = Math.max(textWidth, lineWidth);
-
-                    // start new line
-                    fragments = new ArrayList<>();
-                    fragmentLines.add(fragments);
-                    xAct = 0.0f;
-                    textHeight += lineHeight;
-                    fragments.add(new Fragment(xAct, textHeight, tr.width(), tr.height(), lineBaseLine, f, run));
-                    xAct += tr.width();
-                    lineWidth = tr.width();
-                    whitespace = TextUtil.isBlank(run) ? tr.width() : 0.0f;
-                    lineHeight = tr.height();
-                    wrapAllowed = false;
-                    lineBaseLine = tr.height() + tr.yMin();
-                } else {
-                    wrapAllowed = wrap;
-                    fragments.add(new Fragment(xAct, textHeight, tr.width(), tr.height(), lineBaseLine, f, run));
-                    xAct += tr.width();
-                    lineWidth += tr.width();
-                    whitespace += TextUtil.isBlank(run) ? tr.width() : 0.0f;
-                    lineHeight = Math.max(lineHeight, tr.height());
-                    lineBaseLine = Math.max(lineBaseLine, tr.height() + tr.yMin());
-                }
-            }
-            lineWidth = applyHAlign(fragments, hAlign, width, lineWidth, whitespace);
-            textWidth = Math.max(textWidth, lineWidth);
-            textHeight += lineHeight;
-            baseLine = lineBaseLine;
-        }
-
-        // fix the x-position for justified layout (the last line should be left aligned)
-        if (hAlign == Alignment.JUSTIFY && !fragmentLines.isEmpty()) {
-            List<Fragment> lastLine = fragmentLines.get(fragmentLines.size() - 1);
-            if (!lastLine.isEmpty()) {
-                float dx = -lastLine.get(0).x();
-                lastLine.replaceAll(fragment -> fragment.translate(dx, 0));
-            }
-        }
-
-        // apply anchor and vertical alignment
-        float tx = switch(hAnchor) {
-            case LEFT -> 0.0f;
-            case RIGHT -> -width;
-            case CENTER -> -width/2.0f;
-        };
-        float ty = switch (vAnchor) {
-            case TOP -> 0.0f;
-            case MIDDLE -> -textHeight / 2.0f;
-            case BOTTOM -> -textHeight;
-            case BASELINE -> -textHeight + baseLine;
-        };
-
-        float verticalSpace = Math.max(0, height - textHeight);
-        switch (vAlign) {
-            case TOP -> translateFragments(fragmentLines, tx, ty);
-            case MIDDLE -> translateFragments(fragmentLines, tx, ty - verticalSpace / 2.0f);
-            case BOTTOM -> translateFragments(fragmentLines, tx, ty - verticalSpace);
-            case DISTRIBUTED -> {
-                int n = fragmentLines.size();
-                float k = n > 1 ? verticalSpace / (n - 1) : 0.0f;
-                for (int i = 0; i < n; i++) {
-                    float dy = i * k;
-                    fragmentLines.get(i).replaceAll(fragment -> fragment.translate(tx, ty + dy));
-                }
-                textHeight += (n - 1) * k;
-            }
-        }
-
-        return new FragmentedText(
-                fragmentLines,
-                textWidth,
-                textHeight,
-                baseLine,
-                Math.max(textWidth, width),
-                Math.max(textHeight, height)
-        );
-    }
-
-    /**
-     * Translates all the fragments within the provided list of fragment lines by the specified
-     * horizontal and vertical offsets. The method skips translation if both offsets are zero.
-     *
-     * @param fragmentLines a list of fragment lines where each line is a list of fragments to be translated
-     * @param dx the horizontal offset by which the fragments should be translated
-     * @param dy the vertical offset by which the fragments should be translated
-     */
-    private static void translateFragments(List<List<Fragment>> fragmentLines, float dx, float dy) {
-        if (dx == 0.0f && dy == 0.0f) {
-            return;
-        }
-
-        for (List<Fragment> line: fragmentLines) {
-            line.replaceAll(fragment -> fragment.translate(dx, dy));
-        }
-    }
-
-    /**
-     * Applies horizontal alignment to a list of text fragments according to the specified alignment type.
-     * Adjusts the position and size of the fragments within the given line width and whitespace parameters.
-     *
-     * <p><strong>NOTE: </strong>The method assumes lines start at x-coordinate 0.
-     *
-     * @param line       the list of fragments representing the line of text to be aligned
-     * @param hAlign     the horizontal alignment type (e.g., LEFT, RIGHT, CENTER, JUSTIFY)
-     * @param width      the total width available for the line
-     * @param lineWidth  the current width of the line before alignment
-     * @param whitespace the amount of whitespace available for distribution in the line
-     * @return the new width of the line after alignment adjustments
-     */
-    private static float applyHAlign(List<Fragment> line, Alignment hAlign, float width, float lineWidth, float whitespace) {
-        float availableSpace = Math.max(0.0f, width - lineWidth);
-        float f = whitespace > 0 ? 1.0f + availableSpace / whitespace : 1.0f;
-        switch (hAlign) {
-            case LEFT -> {
-                // nothing to do
-            }
-            case RIGHT -> {
-                line.replaceAll(fragment -> fragment.translate(availableSpace, 0.0f));
-            }
-            case CENTER -> {
-                line.replaceAll(fragment -> fragment.translate(availableSpace / 2.0f, 0.0f));
-            }
-            case JUSTIFY -> {
-                // distribute the remaining space by evenly expanding existing whitespace
-                float x = 0.0f;
-                for (int i = 0; i < line.size(); i++) {
-                    Fragment original = line.get(i);
-                    float w = original.w() * (TextUtil.isBlank(original.text()) ? f : 1.0f);
-                    Fragment aligned = new Fragment(
-                            x,
-                            original.y(),
-                            w,
-                            original.h(),
-                            original.baseLine(),
-                            original.font(),
-                            original.text()
-                    );
-                    line.set(i, aligned);
-                    x += w;
-                }
-            }
-        }
-
-        if (line.isEmpty()) {
-            return 0.0f;
-        } else {
-            Fragment first = line.get(0);
-            Fragment last = line.get(line.size()-1);
-            return last.x() + last.w();
-        }
-    }
 
     /**
      * Renders text fragments within the specified bounding rectangle. The text fragments are provided as a list of fragment lines.
@@ -1018,15 +720,15 @@ public interface Graphics extends AutoCloseable {
             }
         }
 
-        List<List<Fragment>> lines = text.lines;
+        List<List<FragmentedText.Fragment>> lines = text.lines();
 
         for (int i = 0; i < lines.size(); i++) {
-            List<Fragment> fragments = lines.get(i);
+            List<FragmentedText.Fragment> fragments = lines.get(i);
 
-            for (Fragment fragment : fragments) {
-                setFont(fragment.font);
+            for (FragmentedText.Fragment fragment : fragments) {
+                setFont(fragment.font());
                 drawText(
-                        fragment.text.toString(),
+                        fragment.text().toString(),
                         fragment.x() + sx_y * fragment.y() + sx_h * fragment.h(),
                         fragment.y(),
                         HAnchor.LEFT,
@@ -1039,24 +741,6 @@ public interface Graphics extends AutoCloseable {
         setFont(font);
     }
 
-    /**
-     * Splits a RichText line into fragments, preserving whitespace if wrapping is enabled.
-     * Each fragment is either whitespace or text with uniform attributes (font, text decoration).
-     * For each line, a list of such fragments is generated and returned.
-     *
-     * @param line      the RichText line to be split
-     * @param wrapping  a boolean value indicating whether wrapping should be applied
-     * @return a List of Run fragments representing the split line
-     */
-    private static List<Run> splitLinePreservingWhitespace(RichText line, boolean wrapping) {
-        if (!wrapping) {
-            return line.runs();
-        }
-// fixme use pattern
-        return Arrays.stream(line.split("(?<=\\s)|(?=\\s)"))
-                .flatMap(part -> part.runs().stream())
-                .toList();
-    }
 
     @Override
     void close();
