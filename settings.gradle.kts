5b8d212b9a9ed49e9341ca0fcec9bbe9f9c95620
Axel Howind <axh@dua3.com>
2025-01-29 11:16:32 +0100

    v16

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "16-SNAPSHOT"
+val projectVersion = "16"
 
 // define subprojects
 include("utility")
