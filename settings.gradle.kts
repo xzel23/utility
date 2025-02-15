80695b96b5b4d71c0d33ec3bcd0248838282986b
Axel Howind <axh@dua3.com>
2025-02-15 22:05:48 +0100

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "16.0.2"
+val projectVersion = "16.0.3-SNAPSHOT"
 
 // define subprojects
 include("utility")
