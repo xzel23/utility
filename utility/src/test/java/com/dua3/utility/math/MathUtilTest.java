2e0766ffafa1c0077b6b405fe7799366f2ad1106
Axel Howind <axh@dua3.com>
2025-03-12 10:07:56 +0100

    add more MathUtil tests

--- utility/src/test/java/com/dua3/utility/math/MathUtilTest.java
+++ utility/src/test/java/com/dua3/utility/math/MathUtilTest.java
@@ -39,7 +39,7 @@ public class MathUtilTest {
      */
     @Test
     public void testFindRoot() {
-        System.out.println("findRoot");
+
         double result = MathUtil.findRoot(x -> (x - 5) * (x + 2), 4, 10, 1.0e-15);
         double expResult = 5;
         assertEquals(expResult, result, 1.0e-15);
@@ -50,7 +50,7 @@ public class MathUtilTest {
      */
     @Test
     public void testFindRootsInInterval() {
-        System.out.println("findRootsInInterval");
+
         List<Double> result = MathUtil.findRootsInInterval(x -> 3 * x * (x - 2.0 / 3.0), -10.5, +10.5, 20, 1.0e-15);
         assertEquals(2, result.size());
         assertEquals(0, result.get(0), 1.0e-15);
@@ -62,7 +62,7 @@ public class MathUtilTest {
      */
     @Test
     public void testIlog10() {
-        System.out.println("ilog10");
+
         assertEquals(-4, MathUtil.ilog10(0.00099), 1.0e-15);
         assertEquals(-3, MathUtil.ilog10(0.001), 1.0e-15);
         assertEquals(-3, MathUtil.ilog10(0.0099), 1.0e-15);
@@ -104,7 +104,7 @@ public class MathUtilTest {
      */
     @Test
     public void testPow10() {
-        System.out.println("pow10");
+
         assertEquals(0.01, MathUtil.pow10(-2), 1.0e-15);
         assertEquals(0.1, MathUtil.pow10(-1), 1.0e-15);
         assertEquals(1.0, MathUtil.pow10(0), 1.0e-15);
@@ -117,7 +117,7 @@ public class MathUtilTest {
      */
     @Test
     public void testRound() {
-        System.out.println("round");
+
 
         // positive n
         assertEquals(1.2, MathUtil.round(1.23, 1), 1.0e-10);
@@ -159,7 +159,7 @@ public class MathUtilTest {
      */
     @Test
     public void testRoundToPrecision() {
-        System.out.println("roundToPrecision");
+
         assertEquals(1.2, MathUtil.roundToPrecision(1.23, 2), 1.0e-10);
         assertEquals(12.0, MathUtil.roundToPrecision(12.3, 2), 1.0e-10);
         assertEquals(120, MathUtil.roundToPrecision(123, 2), 1.0e-10);
@@ -182,8 +182,6 @@ public class MathUtilTest {
      */
     @Test
     public void testRoundingOperation() {
-        System.out.println("roundingOperation");
-
         // create operations
         Map<RoundingMode, DoubleUnaryOperator> operations = new EnumMap<>(RoundingMode.class);
         for (RoundingMode mode : RoundingMode.values()) {
@@ -297,5 +295,265 @@ public class MathUtilTest {
         assertEquals(6L, MathUtil.gcd(-48L, 18L));
         assertEquals(6L, MathUtil.gcd(48L, -18L));
         assertEquals(6L, MathUtil.gcd(-48L, -18L));
+    }
+
+    @Test
+    public void testRad() {
+        assertEquals(0.0, MathUtil.rad(0.0), 1.0e-15);
+        assertEquals(Math.PI / 4, MathUtil.rad(45.0), 1.0e-15);
+        assertEquals(Math.PI / 2, MathUtil.rad(90.0), 1.0e-15);
+        assertEquals(Math.PI, MathUtil.rad(180.0), 1.0e-15);
+        assertEquals(3 * Math.PI / 2, MathUtil.rad(270.0), 1.0e-15);
+        assertEquals(2 * Math.PI, MathUtil.rad(360.0), 1.0e-15);
+        assertEquals(-Math.PI / 2, MathUtil.rad(-90.0), 1.0e-15);
+    }
+
+    @Test
+    public void testRadf() {
+        assertEquals(0.0f, MathUtil.radf(0.0f), 1.0e-6);
+        assertEquals((float) Math.PI / 4, MathUtil.radf(45.0f), 1.0e-6);
+        assertEquals((float) Math.PI / 2, MathUtil.radf(90.0f), 1.0e-6);
+        assertEquals((float) Math.PI, MathUtil.radf(180.0f), 1.0e-6);
+        assertEquals(3 * (float) Math.PI / 2, MathUtil.radf(270.0f), 1.0e-6);
+        assertEquals(2 * (float) Math.PI, MathUtil.radf(360.0f), 1.0e-6);
+        assertEquals(-(float) Math.PI / 2, MathUtil.radf(-90.0f), 1.0e-6);
+    }
+
+    @Test
+    public void testDeg() {
+        assertEquals(0.0, MathUtil.deg(0.0), 1.0e-15);
+        assertEquals(45.0, MathUtil.deg(Math.PI / 4), 1.0e-15);
+        assertEquals(90.0, MathUtil.deg(Math.PI / 2), 1.0e-15);
+        assertEquals(180.0, MathUtil.deg(Math.PI), 1.0e-15);
+        assertEquals(270.0, MathUtil.deg(3 * Math.PI / 2), 1.0e-15);
+        assertEquals(360.0, MathUtil.deg(2 * Math.PI), 1.0e-15);
+        assertEquals(-90.0, MathUtil.deg(-Math.PI / 2), 1.0e-15);
+    }
+
+    @Test
+    public void testDegf() {
+        assertEquals(0.0f, MathUtil.degf(0.0f), 1.0e-6);
+        assertEquals(45.0f, MathUtil.degf((float) Math.PI / 4), 1.0e-6);
+        assertEquals(90.0f, MathUtil.degf((float) Math.PI / 2), 1.0e-6);
+        assertEquals(180.0f, MathUtil.degf((float) Math.PI), 1.0e-6);
+        assertEquals(270.0f, MathUtil.degf(3 * (float) Math.PI / 2), 1.0e-6);
+        assertEquals(360.0f, MathUtil.degf(2 * (float) Math.PI), 1.0e-6);
+        assertEquals(-90.0f, MathUtil.degf(-(float) Math.PI / 2), 1.0e-6);
+    }
+
+    @Test
+    public void testNormalizeRadians() {
+        assertEquals(0.0, MathUtil.normalizeRadians(0.0), 1.0e-15);
+        assertEquals(Math.PI / 4, MathUtil.normalizeRadians(Math.PI / 4), 1.0e-15);
+        assertEquals(Math.PI / 2, MathUtil.normalizeRadians(Math.PI / 2), 1.0e-15);
+        assertEquals(Math.PI, MathUtil.normalizeRadians(Math.PI), 1.0e-15);
+        assertEquals(3 * Math.PI / 2, MathUtil.normalizeRadians(3 * Math.PI / 2), 1.0e-15);
+        assertEquals(0.0, MathUtil.normalizeRadians(2 * Math.PI), 1.0e-15);
+        assertEquals(Math.PI / 2, MathUtil.normalizeRadians(5 * Math.PI / 2), 1.0e-15);
+        assertEquals(Math.PI / 2, MathUtil.normalizeRadians(-3 * Math.PI / 2), 1.0e-15);
+        assertEquals(3 * Math.PI / 2, MathUtil.normalizeRadians(-Math.PI / 2), 1.0e-15);
+    }
+
+    @Test
+    public void testNormalizeDegrees() {
+        assertEquals(0.0, MathUtil.normalizeDegrees(0.0), 1.0e-15);
+        assertEquals(45.0, MathUtil.normalizeDegrees(45.0), 1.0e-15);
+        assertEquals(90.0, MathUtil.normalizeDegrees(90.0), 1.0e-15);
+        assertEquals(180.0, MathUtil.normalizeDegrees(180.0), 1.0e-15);
+        assertEquals(270.0, MathUtil.normalizeDegrees(270.0), 1.0e-15);
+        assertEquals(0.0, MathUtil.normalizeDegrees(360.0), 1.0e-15);
+        assertEquals(90.0, MathUtil.normalizeDegrees(450.0), 1.0e-15);
+        assertEquals(90.0, MathUtil.normalizeDegrees(-270.0), 1.0e-15);
+        assertEquals(270.0, MathUtil.normalizeDegrees(-90.0), 1.0e-15);
+    }
+
+    @Test
+    public void testQuadrantRadians() {
+        assertEquals(1, MathUtil.quadrantRadians(0.0));
+        assertEquals(1, MathUtil.quadrantRadians(Math.PI / 4));
+        assertEquals(2, MathUtil.quadrantRadians(Math.PI / 2));
+        assertEquals(2, MathUtil.quadrantRadians(3 * Math.PI / 4));
+        assertEquals(3, MathUtil.quadrantRadians(Math.PI));
+        assertEquals(3, MathUtil.quadrantRadians(5 * Math.PI / 4));
+        assertEquals(4, MathUtil.quadrantRadians(3 * Math.PI / 2));
+        assertEquals(4, MathUtil.quadrantRadians(7 * Math.PI / 4));
+        assertEquals(1, MathUtil.quadrantRadians(2 * Math.PI));
+
+        // Test with angles outside [0, 2π)
+        assertEquals(1, MathUtil.quadrantRadians(8 * Math.PI / 4));
+        assertEquals(2, MathUtil.quadrantRadians(10 * Math.PI / 4));
+        assertEquals(1, MathUtil.quadrantRadians(-8 * Math.PI / 4));
+        assertEquals(4, MathUtil.quadrantRadians(-Math.PI / 4));
+    }
+
+    @Test
+    public void testQuadrantIndexRadians() {
+
+        assertEquals(0, MathUtil.quadrantIndexRadians(0.0));
+        assertEquals(0, MathUtil.quadrantIndexRadians(Math.PI / 4));
+        assertEquals(1, MathUtil.quadrantIndexRadians(Math.PI / 2));
+        assertEquals(1, MathUtil.quadrantIndexRadians(3 * Math.PI / 4));
+        assertEquals(2, MathUtil.quadrantIndexRadians(Math.PI));
+        assertEquals(2, MathUtil.quadrantIndexRadians(5 * Math.PI / 4));
+        assertEquals(3, MathUtil.quadrantIndexRadians(3 * Math.PI / 2));
+        assertEquals(3, MathUtil.quadrantIndexRadians(7 * Math.PI / 4));
+        assertEquals(0, MathUtil.quadrantIndexRadians(2 * Math.PI));
+
+        // Test with angles outside [0, 2π)
+        assertEquals(0, MathUtil.quadrantIndexRadians(8 * Math.PI / 4));
+        assertEquals(1, MathUtil.quadrantIndexRadians(10 * Math.PI / 4));
+        assertEquals(0, MathUtil.quadrantIndexRadians(-8 * Math.PI / 4));
+        assertEquals(3, MathUtil.quadrantIndexRadians(-Math.PI / 4));
+    }
+
+    @Test
+    public void testQuadrantDegrees() {
+
+        assertEquals(1, MathUtil.quadrantDegrees(0.0));
+        assertEquals(1, MathUtil.quadrantDegrees(45.0));
+        assertEquals(2, MathUtil.quadrantDegrees(90.0));
+        assertEquals(2, MathUtil.quadrantDegrees(135.0));
+        assertEquals(3, MathUtil.quadrantDegrees(180.0));
+        assertEquals(3, MathUtil.quadrantDegrees(225.0));
+        assertEquals(4, MathUtil.quadrantDegrees(270.0));
+        assertEquals(4, MathUtil.quadrantDegrees(315.0));
+        assertEquals(1, MathUtil.quadrantDegrees(360.0));
+
+        // Test with angles outside [0, 360)
+        assertEquals(1, MathUtil.quadrantDegrees(720.0));
+        assertEquals(2, MathUtil.quadrantDegrees(450.0));
+        assertEquals(1, MathUtil.quadrantDegrees(-720.0));
+        assertEquals(4, MathUtil.quadrantDegrees(-45.0));
+    }
+
+    @Test
+    public void testQuadrantIndexDegrees() {
+
+        assertEquals(0, MathUtil.quadrantIndexDegrees(0.0));
+        assertEquals(0, MathUtil.quadrantIndexDegrees(45.0));
+        assertEquals(1, MathUtil.quadrantIndexDegrees(90.0));
+        assertEquals(1, MathUtil.quadrantIndexDegrees(135.0));
+        assertEquals(2, MathUtil.quadrantIndexDegrees(180.0));
+        assertEquals(2, MathUtil.quadrantIndexDegrees(225.0));
+        assertEquals(3, MathUtil.quadrantIndexDegrees(270.0));
+        assertEquals(3, MathUtil.quadrantIndexDegrees(315.0));
+        assertEquals(0, MathUtil.quadrantIndexDegrees(360.0));
+
+        // Test with angles outside [0, 360)
+        assertEquals(0, MathUtil.quadrantIndexDegrees(720.0));
+        assertEquals(1, MathUtil.quadrantIndexDegrees(450.0));
+        assertEquals(0, MathUtil.quadrantIndexDegrees(-720.0));
+        assertEquals(3, MathUtil.quadrantIndexDegrees(-45.0));
+    }
+
+    @Test
+    public void testOctantRadians() {
+
+        assertEquals(1, MathUtil.octantRadians(0.0));
+        assertEquals(1, MathUtil.octantRadians(Math.PI / 8));
+        assertEquals(2, MathUtil.octantRadians(Math.PI / 4));
+        assertEquals(2, MathUtil.octantRadians(3 * Math.PI / 8));
+        assertEquals(3, MathUtil.octantRadians(Math.PI / 2));
+        assertEquals(3, MathUtil.octantRadians(5 * Math.PI / 8));
+        assertEquals(4, MathUtil.octantRadians(3 * Math.PI / 4));
+        assertEquals(4, MathUtil.octantRadians(7 * Math.PI / 8));
+        assertEquals(5, MathUtil.octantRadians(Math.PI));
+        assertEquals(5, MathUtil.octantRadians(9 * Math.PI / 8));
+        assertEquals(6, MathUtil.octantRadians(5 * Math.PI / 4));
+        assertEquals(6, MathUtil.octantRadians(11 * Math.PI / 8));
+        assertEquals(7, MathUtil.octantRadians(3 * Math.PI / 2));
+        assertEquals(7, MathUtil.octantRadians(13 * Math.PI / 8));
+        assertEquals(8, MathUtil.octantRadians(7 * Math.PI / 4));
+        assertEquals(8, MathUtil.octantRadians(15 * Math.PI / 8));
+        assertEquals(1, MathUtil.octantRadians(2 * Math.PI));
+
+        // Test with angles outside [0, 2π)
+        assertEquals(1, MathUtil.octantRadians(16 * Math.PI / 8));
+        assertEquals(2, MathUtil.octantRadians(18 * Math.PI / 8));
+        assertEquals(1, MathUtil.octantRadians(-16 * Math.PI / 8));
+        assertEquals(8, MathUtil.octantRadians(-Math.PI / 8));
+    }
+
+    @Test
+    public void testOctantIndexRadians() {
+
+        assertEquals(0, MathUtil.octantIndexRadians(0.0));
+        assertEquals(0, MathUtil.octantIndexRadians(Math.PI / 8));
+        assertEquals(1, MathUtil.octantIndexRadians(Math.PI / 4));
+        assertEquals(1, MathUtil.octantIndexRadians(3 * Math.PI / 8));
+        assertEquals(2, MathUtil.octantIndexRadians(Math.PI / 2));
+        assertEquals(2, MathUtil.octantIndexRadians(5 * Math.PI / 8));
+        assertEquals(3, MathUtil.octantIndexRadians(3 * Math.PI / 4));
+        assertEquals(3, MathUtil.octantIndexRadians(7 * Math.PI / 8));
+        assertEquals(4, MathUtil.octantIndexRadians(Math.PI));
+        assertEquals(4, MathUtil.octantIndexRadians(9 * Math.PI / 8));
+        assertEquals(5, MathUtil.octantIndexRadians(5 * Math.PI / 4));
+        assertEquals(5, MathUtil.octantIndexRadians(11 * Math.PI / 8));
+        assertEquals(6, MathUtil.octantIndexRadians(3 * Math.PI / 2));
+        assertEquals(6, MathUtil.octantIndexRadians(13 * Math.PI / 8));
+        assertEquals(7, MathUtil.octantIndexRadians(7 * Math.PI / 4));
+        assertEquals(7, MathUtil.octantIndexRadians(15 * Math.PI / 8));
+        assertEquals(0, MathUtil.octantIndexRadians(2 * Math.PI));
+
+        // Test with angles outside [0, 2π)
+        assertEquals(0, MathUtil.octantIndexRadians(16 * Math.PI / 8));
+        assertEquals(1, MathUtil.octantIndexRadians(18 * Math.PI / 8));
+        assertEquals(0, MathUtil.octantIndexRadians(-16 * Math.PI / 8));
+        assertEquals(7, MathUtil.octantIndexRadians(-Math.PI / 8));
+    }
+
+    @Test
+    public void testOctantDegrees() {
+
+        assertEquals(1, MathUtil.octantDegrees(0.0));
+        assertEquals(1, MathUtil.octantDegrees(22.5));
+        assertEquals(2, MathUtil.octantDegrees(45.0));
+        assertEquals(2, MathUtil.octantDegrees(67.5));
+        assertEquals(3, MathUtil.octantDegrees(90.0));
+        assertEquals(3, MathUtil.octantDegrees(112.5));
+        assertEquals(4, MathUtil.octantDegrees(135.0));
+        assertEquals(4, MathUtil.octantDegrees(157.5));
+        assertEquals(5, MathUtil.octantDegrees(180.0));
+        assertEquals(5, MathUtil.octantDegrees(202.5));
+        assertEquals(6, MathUtil.octantDegrees(225.0));
+        assertEquals(6, MathUtil.octantDegrees(247.5));
+        assertEquals(7, MathUtil.octantDegrees(270.0));
+        assertEquals(7, MathUtil.octantDegrees(292.5));
+        assertEquals(8, MathUtil.octantDegrees(315.0));
+        assertEquals(8, MathUtil.octantDegrees(337.5));
+        assertEquals(1, MathUtil.octantDegrees(360.0));
+
+        // Test with angles outside [0, 360)
+        assertEquals(1, MathUtil.octantDegrees(720.0));
+        assertEquals(2, MathUtil.octantDegrees(405.0));
+        assertEquals(1, MathUtil.octantDegrees(-720.0));
+        assertEquals(8, MathUtil.octantDegrees(-22.5));
+    }
+
+    @Test
+    public void testOctantIndexDegrees() {
+        assertEquals(0, MathUtil.octantIndexDegrees(0.0));
+        assertEquals(0, MathUtil.octantIndexDegrees(22.5));
+        assertEquals(1, MathUtil.octantIndexDegrees(45.0));
+        assertEquals(1, MathUtil.octantIndexDegrees(67.5));
+        assertEquals(2, MathUtil.octantIndexDegrees(90.0));
+        assertEquals(2, MathUtil.octantIndexDegrees(112.5));
+        assertEquals(3, MathUtil.octantIndexDegrees(135.0));
+        assertEquals(3, MathUtil.octantIndexDegrees(157.5));
+        assertEquals(4, MathUtil.octantIndexDegrees(180.0));
+        assertEquals(4, MathUtil.octantIndexDegrees(202.5));
+        assertEquals(5, MathUtil.octantIndexDegrees(225.0));
+        assertEquals(5, MathUtil.octantIndexDegrees(247.5));
+        assertEquals(6, MathUtil.octantIndexDegrees(270.0));
+        assertEquals(6, MathUtil.octantIndexDegrees(292.5));
+        assertEquals(7, MathUtil.octantIndexDegrees(315.0));
+        assertEquals(7, MathUtil.octantIndexDegrees(337.5));
+        assertEquals(0, MathUtil.octantIndexDegrees(360.0));
+
+        // Test with angles outside [0, 360)
+        assertEquals(0, MathUtil.octantIndexDegrees(720.0));
+        assertEquals(1, MathUtil.octantIndexDegrees(405.0));
+        assertEquals(0, MathUtil.octantIndexDegrees(-720.0));
+        assertEquals(7, MathUtil.octantIndexDegrees(-22.5));
     }
 }
