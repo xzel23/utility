ab3939e1d9c1518e2e2e12e9a58f258c6b8b4c27
Axel Howind <axh@dua3.com>
2025-04-17 09:46:00 +0200

    Dialogs.openFile/saveFileAs()

--- utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/Dialogs.java
+++ utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/Dialogs.java
@@ -14,9 +14,23 @@
 
 package com.dua3.utility.fx.controls;
 
+import com.dua3.utility.io.FileType;
+import com.dua3.utility.io.OpenMode;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.jspecify.annotations.Nullable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Window;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Utility class for creating various types of dialogs and panes.
@@ -194,5 +208,176 @@ public final class Dialogs {
      */
     public static WizardDialogBuilder wizard() {
         return new WizardDialogBuilder();
+    }
+
+    /**
+     * Exception thrown to indicate that an unsupported file type was encountered.
+     * This exception is generally used in scenarios where certain file types are not
+     * suitable for a given operation or are not supported by the application.
+     */
+    public static class UnsupportedFileTypeException extends IOException {
+        private static final long serialVersionUID = 1L;
+        private final @Nullable Path path;
+
+        /**
+         * Constructs a new UnsupportedFileTypeException with a specified detail message.
+         *
+         * @param message the detail message describing the reason for the exception.
+         */
+        public UnsupportedFileTypeException(String message) {
+            this(message, null);
+        }
+
+        /**
+         * Constructs a new {@code UnsupportedFileTypeException} with the specified detail message
+         * and an optional file path associated with the exception.
+         *
+         * @param message the detail message explaining the reason for the exception
+         * @param path the file path associated with the unsupported file type, or {@code null} if not applicable
+         */
+        public UnsupportedFileTypeException(String message, @Nullable Path path) {
+            super(message);
+            this.path = path;
+        }
+
+        /**
+         * Retrieves the file path associated with the exception, if available.
+         *
+         * @return an {@link Optional} containing the file path if it exists, or an empty {@code Optional} if the path is not set.
+         */
+        Optional<Path> getPath() {
+            return Optional.ofNullable(path);
+        }
+    }
+
+    /**
+     * Opens a file selection dialog for the user to choose a file, with support for filtering by file types.
+     * The selected file is then read and returned wrapped in an {@link Optional}, or {@link Optional#empty()} if no file is chosen.
+     * If the file type is unsupported or cannot be determined, an {@link UnsupportedFileTypeException} is thrown.
+     *
+     * @param <T>               The type of object that the selected file will be read into.
+     * @param stage             The parent {@link Stage} for the file chooser dialog.
+     * @param type              The class type of the objects to read from the selected file.
+     * @param defaultFileType   The default file type to preselect in the file chooser, or {@code null} if no default is specified.
+     * @return An {@link Optional} containing an object of type {@code T} read from the selected file,
+     *         or an empty {@link Optional} if no file is selected.
+     * @throws IOException                  If an I/O error occurs while reading the selected file.
+     * @throws UnsupportedFileTypeException If the type of the selected file cannot be determined or is unsupported.
+     */
+    public static <T> Optional<T> openFile(Stage stage, Class<T> type, @Nullable FileType<? extends T> defaultFileType) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+
+        var types = FileType.allReadersForType(type).stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        Dialogs::createExtensionFilter,
+                        (a, b) -> b,
+                        IdentityHashMap::new)
+                );
+
+        fileChooser.getExtensionFilters().addAll(
+                types.values().stream()
+                        .sorted(Comparator.comparing(FileChooser.ExtensionFilter::getDescription))
+                        .toList()
+        );
+        fileChooser.setSelectedExtensionFilter(types.get(defaultFileType));
+
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile == null) {
+            return Optional.empty();
+        }
+
+        FileChooser.ExtensionFilter selectedFilter = fileChooser.getSelectedExtensionFilter();
+        Optional<? extends FileType<? extends T>> fileType = types.entrySet().stream()
+                .filter(entry -> entry.getValue() == selectedFilter)
+                .map(Map.Entry::getKey)
+                .findFirst();
+
+        if (fileType.isEmpty()) {
+            fileType = types.keySet().stream()
+                    .filter(ft -> ft.isSupported(OpenMode.READ))
+                    .filter(ft -> type.isAssignableFrom(ft.getDocumentClass()))
+                    .findFirst();
+        }
+
+        if (fileType.isEmpty()) {
+            throw new UnsupportedFileTypeException("The type of the file could not be determined");
+        }
+
+        return Optional.of(fileType.orElseThrow().read(selectedFile.toPath()));
+    }
+
+    /**
+     * Creates a new {@link FileChooser.ExtensionFilter} instance based on the provided {@link FileType}.
+     *
+     * @param <T> The type that this file type can handle.
+     * @param ft  The file type containing the name and extension patterns to be used for the filter.
+     * @return A new {@link FileChooser.ExtensionFilter} with the specified name and extension patterns.
+     */
+    public static <T> FileChooser.ExtensionFilter createExtensionFilter(FileType<? extends T> ft) {
+        return new FileChooser.ExtensionFilter(ft.getName(), ft.getExtensionPatterns()
+        );
+    }
+
+    /**
+     * Opens a file save dialog to allow the user to save the given object to a file.
+     * The dialog allows filtering by supported file types and preselects a default file type if specified.
+     * The method writes the object's data to the selected file in the appropriate format.
+     *
+     * @param <T>            The type of object to be saved.
+     * @param stage          The parent {@link Stage} for the file chooser dialog.
+     * @param object         The object to be saved to a file.
+     * @param defaultFileType The default file type to preselect in the file chooser, or {@code null} if no default is specified.
+     * @return {@code true} if the object was successfully saved to a file; {@code false} if the user canceled the operation.
+     * @throws IOException If an I/O error occurs during file saving.
+     * @throws UnsupportedFileTypeException If the type of the file cannot be determined or is unsupported.
+     */
+    public static <T> boolean saveToFile(Stage stage, T object, @Nullable FileType<? extends T> defaultFileType) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+
+        @SuppressWarnings("unchecked")
+        Class<T> type = (Class<T>) object.getClass();
+        var types = FileType.allWritersForType(type).stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        Dialogs::createExtensionFilter,
+                        (a, b) -> b,
+                        IdentityHashMap::new)
+                );
+
+        fileChooser.getExtensionFilters().addAll(
+                types.values().stream()
+                        .sorted(Comparator.comparing(FileChooser.ExtensionFilter::getDescription))
+                        .toList()
+        );
+        fileChooser.setSelectedExtensionFilter(types.get(defaultFileType));
+
+        File selectedFile = fileChooser.showSaveDialog(stage);
+
+        if (selectedFile == null) {
+            return false;
+        }
+
+        FileChooser.ExtensionFilter selectedFilter = fileChooser.getSelectedExtensionFilter();
+        Optional<? extends FileType<? super T>> fileType = types.entrySet().stream()
+                .filter(entry -> entry.getValue() == selectedFilter)
+                .map(Map.Entry::getKey)
+                .findFirst();
+
+        if (fileType.isEmpty()) {
+            fileType = types.keySet().stream()
+                    .filter(ft -> ft.isSupported(OpenMode.WRITE))
+                    .filter(ft -> type.isAssignableFrom(ft.getDocumentClass()))
+                    .findFirst();
+        }
+
+        if (fileType.isEmpty()) {
+            throw new UnsupportedFileTypeException("The type of the file could not be determined");
+        }
+
+        fileType.orElseThrow().write(selectedFile.toPath(), object);
+
+        return true;
     }
 }
