17ce3d34b034050ed9af82d959f93f1e2c03bce3
Axel Howind <axh@dua3.com>
2025-04-17 10:29:04 +0200

    added explicit constructors to service classes

--- utility-fx/src/main/java/com/dua3/utility/fx/FxFontUtilProvider.java
+++ utility-fx/src/main/java/com/dua3/utility/fx/FxFontUtilProvider.java
@@ -7,6 +7,14 @@ import com.dua3.utility.text.FontUtilProvider;
  * Implementation of the {@link FontUtilProvider} interface for JavaFX.
  */
 public class FxFontUtilProvider implements FontUtilProvider {
+
+    /**
+     * Default constructor, called by SPI.
+     */
+    public FxFontUtilProvider() {
+        // nothing to do
+    }
+
     @Override
     public FontUtil<?> get() {
         return FxFontUtil.getInstance();
--- utility-fx/src/main/java/com/dua3/utility/fx/FxImageUtilProvider.java
+++ utility-fx/src/main/java/com/dua3/utility/fx/FxImageUtilProvider.java
@@ -7,6 +7,14 @@ import com.dua3.utility.data.ImageUtilProvider;
  * Implementation of the {@link ImageUtilProvider} interface for JavaFX.
  */
 public class FxImageUtilProvider implements ImageUtilProvider {
+
+    /**
+     * Default constructor, called by SPI.
+     */
+    public FxImageUtilProvider() {
+        // nothing to do
+    }
+
     @Override
     public ImageUtil<?> get() {
         return FxImageUtil.getInstance();
