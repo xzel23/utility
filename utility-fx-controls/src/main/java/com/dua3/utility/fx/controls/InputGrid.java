85fca3de8d02761d8a4804e67e74797bb859861a
Axel Howind <axh@dua3.com>
2025-05-25 05:40:10 +0200

    remove import

--- utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/InputGrid.java
+++ utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/InputGrid.java
@@ -16,7 +16,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
