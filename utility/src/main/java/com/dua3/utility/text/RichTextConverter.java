b07c6315d02035e5ab665eb48fb5b91dce985444
Axel Howind <axh@dua3.com>
2025-06-12 12:45:10 +0200

    refactor

--- utility/src/main/java/com/dua3/utility/text/RichTextConverter.java
+++ utility/src/main/java/com/dua3/utility/text/RichTextConverter.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 @FunctionalInterface
 public interface RichTextConverter<T> {
-
+    
     /**
      * Add font properties of to a properties map.
      *
@@ -20,10 +20,7 @@ public interface RichTextConverter<T> {
      */
     static void putFontProperties(Map<? super String, @Nullable Object> props, Font font) {
         props.put(Style.FONT_FAMILIES, font.getFamilies());
-        String fontClass = font.getType() == FontType.MONOSPACED ? Style.FONT_CLASS_VALUE_MONOSPACE
-                : font.getFamily().equals("serif") ? Style.FONT_CLASS_VALUE_SERIF
-                : font.getFamily().equals("sans-serif") ? Style.FONT_CLASS_VALUE_SANS_SERIF
-                : null;
+        String fontClass = determineFontClass(font);
         if (fontClass != null) {
             props.put(Style.FONT_CLASS, fontClass);
         }
@@ -33,6 +30,29 @@ public interface RichTextConverter<T> {
         props.put(Style.FONT_WEIGHT, font.isBold() ? Style.FONT_WEIGHT_VALUE_BOLD : Style.FONT_WEIGHT_VALUE_NORMAL);
         props.put(Style.TEXT_DECORATION_UNDERLINE, font.isUnderline() ? Style.TEXT_DECORATION_UNDERLINE_VALUE_LINE : Style.TEXT_DECORATION_UNDERLINE_VALUE_NO_LINE);
         props.put(Style.TEXT_DECORATION_LINE_THROUGH, font.isStrikeThrough() ? Style.TEXT_DECORATION_LINE_THROUGH_VALUE_LINE : Style.TEXT_DECORATION_LINE_THROUGH_VALUE_NO_LINE);
+    }
+
+    /**
+     * Determines the CSS font class based on font type and family.
+     *
+     * @param font the font to analyze
+     * @return the appropriate font class constant, or null if no specific class applies
+     */
+    private static @Nullable String determineFontClass(Font font) {
+        if (font.getType() == FontType.MONOSPACED) {
+            return Style.FONT_CLASS_VALUE_MONOSPACE;
+        }
+
+        String family = font.getFamily();
+        if ("serif".equals(family)) {
+            return Style.FONT_CLASS_VALUE_SERIF;
+        }
+
+        if ("sans-serif".equals(family)) {
+            return Style.FONT_CLASS_VALUE_SANS_SERIF;
+        }
+
+        return null;
     }
 
     /**
