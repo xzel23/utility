86c8b1122c03a173ced54dc6f4155ae1377dda37
Axel Howind <axh@dua3.com>
2025-06-22 20:22:40 +0200

    sonar issue

--- utility-samples/utility-samples-bom/src/main/java/com/dua3/utility/samples/bom/BomSample.java
+++ utility-samples/utility-samples-bom/src/main/java/com/dua3/utility/samples/bom/BomSample.java
@@ -1,6 +1,5 @@
 package com.dua3.utility.samples.bom;
 
-import com.dua3.utility.lang.LangUtil;
 import com.dua3.utility.logging.LogUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,9 +7,17 @@ import org.apache.logging.log4j.Logger;
 /**
  * A simple sample demonstrating the use of utility libraries with the utility-bom.
  */
-public class BomSample {
+public final class BomSample {
+    private BomSample() { /* utility class */ }
+
     private static final Logger LOG = LogManager.getLogger(BomSample.class);
 
+    /**
+     * The entry point of the application. This method initializes the logging system, processes
+     * text by reversing it, and logs the results at various stages.
+     *
+     * @param args command-line arguments passed to the application
+     */
     public static void main(String[] args) {
         // Initialize logging
         LogUtil.assureInitialized();
