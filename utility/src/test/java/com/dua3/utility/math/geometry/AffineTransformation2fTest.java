aee146330863c14ce97b0c11b02ae513b373a746
Axel Howind <axh@dua3.com>
2025-07-15 05:59:31 +0200

    remove import

--- utility/src/test/java/com/dua3/utility/math/geometry/AffineTransformation2fTest.java
+++ utility/src/test/java/com/dua3/utility/math/geometry/AffineTransformation2fTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SuppressWarnings("UnnecessaryLocalVariable")
