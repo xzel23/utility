a1cea05bebdccfa6a0fe95a0908b112726be04b6
Axel Howind <axh@dua3.com>
2025-07-16 08:41:52 +0200

    Add package info

---
+++ utility/src/main/java/com/dua3/utility/crypt/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains cryptographic utilities and related class.
+ * <p>
+ * For some functionality that requires X509 certificates, make sure to include a dependency on Bouncycyastle
+ * in your build.
+ */
+package com.dua3.utility.crypt;
