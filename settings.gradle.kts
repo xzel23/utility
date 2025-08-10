e25760df172220e79d38ee84c3ae0a35f96e061c
Axel Howind <axh@dua3.com>
2025-08-10 09:33:47 +0200

    v20.0.0-beta9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "20.0.0-beta9-SNAPSHOT"
+val projectVersion = "20.0.0-beta9"
 
 // define subprojects
 include("utility")
