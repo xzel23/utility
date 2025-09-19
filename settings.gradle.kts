9953152dfd811e5709cb7ac9449957cf0b80e856
Axel Howind <axh@dua3.com>
2025-09-19 09:40:13 +0200

    update gradle wrapper, dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "20.0.0-beta17-SNAPSHOT"
+val projectVersion = "20.0.0-beta17"
 
 // define subprojects
 include("utility")
@@ -50,7 +50,7 @@ dependencyResolutionManagement {
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
             plugin("jmh", "me.champeau.jmh").version("0.7.3")
             plugin("jreleaser", "org.jreleaser").version("1.20.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.4.1")
+            plugin("spotbugs", "com.github.spotbugs").version("6.4.2")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
 
@@ -64,8 +64,8 @@ dependencyResolutionManagement {
             version("log4j-bom", "2.25.1")
             version("miglayout", "11.4.2")
             version("slf4j", "2.0.17")
-            version("spotbugs", "4.9.5")
-            version("bouncycastle", "1.81")
+            version("spotbugs", "4.9.6")
+            version("bouncycastle", "1.82")
             version("utility-bom", projectVersion)
 
             library("jspecify", "org.jspecify", "jspecify").versionRef("jspecify")
