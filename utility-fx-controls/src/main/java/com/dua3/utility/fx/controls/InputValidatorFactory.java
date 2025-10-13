b84d310fdfb5f6171a4557c5c1bc4456e8838ad1
Axel Howind <axh@dua3.com>
2025-10-13 02:50:16 +0200

    add more validation methods

--- utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/InputValidatorFactory.java
+++ utility-fx-controls/src/main/java/com/dua3/utility/fx/controls/InputValidatorFactory.java
@@ -1,8 +1,12 @@
 package com.dua3.utility.fx.controls;
 
+import com.dua3.utility.io.IoUtil;
 import com.dua3.utility.text.MessageFormatter;
 import org.jspecify.annotations.Nullable;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
@@ -34,35 +38,6 @@ public class InputValidatorFactory {
     }
 
     /**
-     * Creates a validation function that checks if a string is non-empty.
-     * If the string is empty, the provided error message format and arguments are used to create an error message.
-     *
-     * @param fmt the format string for the error message if validation fails
-     * @param args optional arguments for formatting the error message
-     * @return a function that takes a string and returns an {@code Optional<String>} containing
-     *         the error message if the string is empty, or an empty {@code Optional} if it is valid
-     */
-    public Function<String, Optional<String>> nonEmpty(String fmt, Object... args) {
-        return s -> validate(Predicate.not(String::isEmpty), s, fmt, args);
-    }
-
-    /**
-     * Creates a validation function that checks if an input string matches a given regular expression pattern.
-     * If the input matches the pattern, the function returns {@code Optional.empty()}.
-     * Otherwise, it returns an {@code Optional} containing a formatted error message.
-     *
-     * @param pattern the regular expression pattern to match against the input string
-     * @param fmt the format string for the error message to be returned on validation failure
-     * @param args optional arguments to include in the formatted error message
-     * @return a function that takes a string as input and returns an {@code Optional<String>} containing
-     *         a validation error message if the input does not match the regular expression;
-     *         otherwise {@code Optional.empty()} is returned
-     */
-    public Function<String, Optional<String>> regexp(String pattern, String fmt, Object... args) {
-        return s -> validate(Pattern.compile(pattern).asMatchPredicate(), s, fmt, args);
-    }
-
-    /**
      * Creates a validation function that checks if an input value is non-null.
      * If the input is null, the provided error message format and arguments are used to create an error message.
      *
@@ -77,6 +52,114 @@ public class InputValidatorFactory {
     }
 
     /**
+     * Creates a validation function that checks if a string is non-empty.
+     * If the string is empty, the provided error message format and arguments are used to create an error message.
+     *
+     * @param fmt the format string for the error message if validation fails
+     * @param args optional arguments for formatting the error message
+     * @return a function that takes a string and returns an {@code Optional<String>} containing
+     *         the error message if the string is null or empty, or an empty {@code Optional} if it is valid
+     */
+    public Function<@Nullable String, Optional<String>> nonEmpty(String fmt, Object... args) {
+        return s -> validate(v -> v != null && !v.isEmpty(), s, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if a string is non-empty.
+     * If the string is empty, the provided error message format and arguments are used to create an error message.
+     *
+     * @param fmt the format string for the error message if validation fails
+     * @param args optional arguments for formatting the error message
+     * @return a function that takes a string and returns an {@code Optional<String>} containing
+     *         the error message if the string is null or blank, or an empty {@code Optional} if it is valid
+     */
+    public Function<@Nullable String, Optional<String>> nonBlank(String fmt, Object... args) {
+        return s -> validate(v -> v != null && !v.isBlank(), s, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if an input string matches a given regular expression pattern.
+     * If the input matches the pattern, the function returns {@code Optional.empty()}.
+     * Otherwise, it returns an {@code Optional} containing a formatted error message.
+     *
+     * @param pattern the regular expression pattern to match against the input string
+     * @param fmt the format string for the error message to be returned on validation failure
+     * @param args optional arguments to include in the formatted error message
+     * @return a function that takes a string as input and returns an {@code Optional<String>} containing
+     *         a validation error message if the input is null or does not match the regular expression;
+     *         otherwise {@code Optional.empty()} is returned
+     */
+    public Function<@Nullable String, Optional<String>> regexp(String pattern, String fmt, Object... args) {
+        Predicate<String> matchPredicate = Pattern.compile(pattern).asMatchPredicate();
+        return s -> validate(v -> v != null && matchPredicate.test(v), s, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if a file or directory exists at the specified {@code Path}.
+     * If the {@code Path} does not exist, the provided error message format and arguments are used to create
+     * an error message.
+     *
+     * @param fmt  the format string for the error message if validation fails
+     * @param args optional arguments for formatting the error message
+     * @return a function that takes a {@link Path} and returns an {@code Optional<String>} containing
+     *         the error message if the {@code Path} is null or does not exist, or an empty {@code Optional}
+     *         if it exists
+     */
+    public Function<@Nullable Path, Optional<String>> exists(String fmt, Object... args) {
+        return v -> validate(p -> p != null && Files.exists(p), v, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if the input {@link Path} represents a regular file
+     * in the file system. If the {@code Path} does not refer to a regular file, the provided error
+     * message format and arguments are used to create an error message.
+     *
+     * @param fmt  the format string for the error message to be returned on validation failure
+     * @param args optional arguments to include in the formatted error message
+     * @return a function that takes a {@link Path} and returns an {@code Optional<String>} containing
+     *         a validation error message if the {@code Path} is null, does not exist, or is not a
+     *         regular file, or an empty {@code Optional} if it is valid
+     */
+    public Function<@Nullable Path, Optional<String>> isRegularFile(String fmt, Object... args) {
+        return v -> validate(p -> p != null && Files.isRegularFile(p), v, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if the input {@link Path} represents a regular file
+     * with an extension included in the specified collection of extensions. If the {@code Path} does not
+     * meet these conditions, the provided error message format and arguments are used to create an error
+     * message.
+     * <p>
+     * <strong>Note:</strong> provide the file extensions without a leading dot, i.e.,
+     * to allow text and markdown files, use {@code List.of("txt", "md5")}.
+     *
+     * @param extensions the collection of allowed file extensions to validate against
+     * @param fmt        the format string for the error message to be returned on validation failure
+     * @param args       optional arguments to include in the formatted error message
+     * @return a function that takes a {@link Path} and returns an {@code Optional<String>} containing
+     *         a validation error message if the {@code Path} is null, does not exist, is not a regular
+     *         file, or does not have a valid extension, or an empty {@code Optional} if it is valid
+     */
+    public Function<@Nullable Path, Optional<String>> isRegularFileWithExtension(Collection<String> extensions, String fmt, Object... args) {
+        return v -> validate(p -> p != null && extensions.contains(IoUtil.getExtension(p)) && Files.isRegularFile(p), v, fmt, args);
+    }
+
+    /**
+     * Creates a validation function that checks if the input {@link Path} represents a directory
+     * in the file system. If the {@code Path} is not a directory, the provided error message format
+     * and arguments are used to create an error message.
+     *
+     * @param fmt  the format string for the error message to be returned on validation failure
+     * @param args optional arguments to include in the formatted error message
+     * @return a function that takes a {@link Path} and returns an {@code Optional<String>} containing
+     *         a validation error message if the {@code Path} is null, does not exist, or is not a
+     *         directory, or an empty {@code Optional} if it is valid
+     */
+    public Function<@Nullable Path, Optional<String>> isDirectory(String fmt, Object... args) {
+        return v -> validate(p -> p != null && Files.isDirectory(p), v, fmt, args);
+    }
+
+    /**
      * Validates a given value against a specified predicate and returns an error message
      * if the predicate test fails.
      *
@@ -88,7 +171,7 @@ public class InputValidatorFactory {
      * @return an {@code Optional} containing the formatted error message if validation fails,
      *         or an empty {@code Optional} if validation succeeds
      */
-    private <T> Optional<String> validate(Predicate<T> predicate, T value, String fmt, Object... args) {
+    private <T extends @Nullable Object> Optional<String> validate(Predicate<T> predicate, T value, String fmt, Object... args) {
         if (predicate.test(value)) {
             return Optional.empty();
         } else {
@@ -105,8 +188,8 @@ public class InputValidatorFactory {
      * @param args     additional arguments to be included in the formatted output
      * @return the formatted string with placeholders in the format string replaced by the corresponding arguments
      */
-    private String format(String fmt, Object firstArg, Object... args) {
-        Object[] fmtArgs = new Object[args.length + 1];
+    private String format(String fmt, @Nullable Object firstArg, @Nullable Object... args) {
+        @Nullable Object[] fmtArgs = new Object[args.length + 1];
         fmtArgs[0] = firstArg;
         System.arraycopy(args, 0, fmtArgs, 1, args.length);
         return formatter.format(fmt, fmtArgs);
