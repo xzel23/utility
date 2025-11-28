c32b52348483cf569ac392bba8b1fc6a78b72975
Axel Howind <axh@dua3.com>
2025-11-28 04:12:24 +0100

    bump SpotBugs plugin and BouncyCastle versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -51,7 +51,7 @@ dependencyResolutionManagement {
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
             plugin("jmh", "me.champeau.jmh").version("0.7.3")
             plugin("jreleaser", "org.jreleaser").version("1.21.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.4.5")
+            plugin("spotbugs", "com.github.spotbugs").version("6.4.6")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("versions", "com.github.ben-manes.versions").version("0.53.0")
 
@@ -67,7 +67,7 @@ dependencyResolutionManagement {
             version("miglayout", "11.4.2")
             version("slf4j", "2.0.17")
             version("spotbugs", "4.9.8")
-            version("bouncycastle", "1.82")
+            version("bouncycastle", "1.83")
             version("utility-bom", projectVersion)
 
             library("atlantafx", "io.github.mkpaz", "atlantafx-base").versionRef("atlantafx")
