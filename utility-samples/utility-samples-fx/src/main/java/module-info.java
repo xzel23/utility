a6bc448b64aac6f3e07f002fee035a5f12ca717c
Axel Howind <axh@dua3.com>
2025-12-09 12:54:36 +0100

    update module dependencies in utility-samples-fx module

--- utility-samples/utility-samples-fx/src/main/java/module-info.java
+++ utility-samples/utility-samples-fx/src/main/java/module-info.java
@@ -18,14 +18,13 @@ open module com.dua3.utility.samples.fx {
     requires com.dua3.utility.fx.controls;
     requires com.dua3.utility.fx.icons;
     requires com.dua3.utility.logging;
-    requires org.slf4j;
     requires com.dua3.utility.logging.log4j;
+    requires java.logging;
     requires javafx.controls;
     requires javafx.graphics;
     requires org.apache.commons.logging;
-    requires java.logging;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.jul;
     requires org.jspecify;
-    requires java.prefs; // DO NOT REMOVE! this adds the JUL bridge.
+    requires org.slf4j;
 }
