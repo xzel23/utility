e7a1642db2fa86196230fcc08c17d85962b351f0
Axel Howind <axh@dua3.com>
2026-01-02 01:32:35 +0100

    bump: update project version to 20.4.1-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "20.4.0"
+val projectVersion = "20.4.1-SNAPSHOT"
 
 // define subprojects
 include("utility")
