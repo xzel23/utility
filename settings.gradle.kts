7acd783c67c5eca98be09b3dd6eb0c89e79942ab
Axel Howind <axh@dua3.com>
2026-01-23 02:50:45 +0100

    bump version to 21-rc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "21-beta2-SNAPSHOT"
+val projectVersion = "21-rc"
 
 // define subprojects
 include("utility")
