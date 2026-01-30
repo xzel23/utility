37ad012b27f5f0a9386e924dd85f7742e4b61691
Axel Howind <axh@dua3.com>
2026-01-30 06:33:25 +0100

    Update project version to `21` in build script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "21-rc3"
+val projectVersion = "21"
 
 // define subprojects
 include("utility")
