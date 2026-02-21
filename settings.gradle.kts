3512e63b9ae787bdffcca0e1a909402a6a85569d
Axel Howind <axh@dua3.com>
2026-02-21 18:06:35 +0100

    Set project version to 21.1.0-rc in `settings.gradle.kts`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "21.1.0"
+val projectVersion = "21.1.0-rc"
 
 // define subprojects
 include("utility")
