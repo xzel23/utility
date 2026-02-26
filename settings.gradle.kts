dd726669babb05063b5953b2d6d68967298f8b6d
Axel Howind <axh@dua3.com>
2026-02-26 03:14:21 +0100

    Update project version to 22.0.0-beta2-SNAPSHOT in `settings.gradle.kts`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-utility"
-val projectVersion = "22.0.0-beta"
+val projectVersion = "22.0.0-beta2-SNAPSHOT"
 
 // define subprojects
 include("utility")
